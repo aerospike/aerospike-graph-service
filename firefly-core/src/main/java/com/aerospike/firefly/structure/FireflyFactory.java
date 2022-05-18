aa82cc43983620b67bcd1c7e32ba8f69d9e925e9
Grant <grant@iowntheinter.net>
2022-05-17 23:36:27 -0600

    fix mistake in git merge

--- firefly-core/src/main/java/com/aerospike/firefly/structure/FireflyFactory.java
+++
@@ -1,7 +0,0 @@
-package com.aerospike.firefly.structure;
-
-/**
- * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
- */
-public class FireflyFactory {
-}
--- firefly-core/src/test/java/com/aerospike/firefly/Tokens.java
+++
@@ -1,8 +0,0 @@
-package com.aerospike.firefly;
-
-/**
- * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
- */
-public class Tokens {
-    public static final String INTEGRATION_TEST_PROPERTIES = "phaseshift-integration-settings.properties";
-}

012f335ca9244e6f591dacfbfa7fdf25c335fffd
Grant Haywood <grant@iowntheinter.net>
2022-05-23 23:26:11 -0600

    implement firefly gremlin-console plugin

---
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/jsr223/FireflyGremlinPlugin.java
@@ -0,0 +1,54 @@
+package com.aerospike.firefly.jsr223;
+
+import com.aerospike.firefly.structure.*;
+import com.aerospike.firefly.util.ConfigurationHelper;
+import com.aerospike.firefly.util.Tokens;
+import org.apache.tinkerpop.gremlin.jsr223.AbstractGremlinPlugin;
+import org.apache.tinkerpop.gremlin.jsr223.DefaultImportCustomizer;
+import org.apache.tinkerpop.gremlin.jsr223.ImportCustomizer;
+
+import java.io.ObjectInputFilter;
+
+/**
+ * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
+ */
+public final class FireflyGremlinPlugin extends AbstractGremlinPlugin {
+    private static final String NAME = "tinkerpop.firefly";
+    private static final ImportCustomizer imports;
+
+    static {
+        try {
+            imports = DefaultImportCustomizer.build()
+                    .addClassImports(
+                            FireflyGraph.class,
+                            FireflyGraphFeatures.class,
+                            FireflyGraphVariables.class,
+                            FireflyElement.class,
+                            FireflyEdge.class,
+                            FireflyVertex.class,
+                            FireflyVertexProperty.class,
+                            FireflyProperty.class,
+                            FireflyHelper.class,
+                            ConfigurationHelper.class
+                    ).create();
+        } catch (Exception ex) {
+            System.out.println("ERROR LOADING");
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static final FireflyGremlinPlugin instance = new FireflyGremlinPlugin();
+
+    public FireflyGremlinPlugin() {
+        super(NAME, imports);
+    }
+
+    public static FireflyGremlinPlugin instance() {
+        return instance;
+    }
+
+    @Override
+    public boolean requireRestart() {
+        return true;
+    }
+}

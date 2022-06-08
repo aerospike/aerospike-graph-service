f9b7ed7e2fcddeddfda186ea41fe5c0aacc22ef1
Grant Haywood <grant@iowntheinter.net>
2022-06-07 23:16:12 -0600

    profile utility

---
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/util/Profile.java
@@ -0,0 +1,40 @@
+package com.aerospike.firefly.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
+ */
+public class Profile {
+    protected static Map<String, Map<String, AtomicLong>> metrics = new HashMap<>();
+
+    private Profile() {
+    }
+
+    public static void increment(Class<?> clazz, String functionName) {
+        Map<String, AtomicLong> clazzMetrics = metrics.getOrDefault(clazz.getName(), new HashMap<>());
+        AtomicLong functionMetric = clazzMetrics.getOrDefault(functionName, new AtomicLong());
+        functionMetric.addAndGet(1);
+        clazzMetrics.put(functionName, functionMetric);
+        metrics.put(clazz.getName(), clazzMetrics);
+    }
+
+    public static void reset() {
+        metrics.clear();
+    }
+
+    public static String report() {
+        final List<String> results = new ArrayList<>();
+        metrics.forEach((clazz, clazzMetrics) -> {
+            clazzMetrics.forEach((fn, callCount) -> {
+                results.add(String.format("%s.%s: %d calls", clazz, fn, callCount.get()));
+            });
+        });
+        return results.stream().reduce("", (a, b) -> a + "\n" + b);
+    }
+
+}
---
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/util/TestProfiler.java
@@ -0,0 +1,35 @@
+package com.aerospike.firefly.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
+ */
+public class TestProfiler {
+    @Test
+    public void testCallCounter(){
+        Profile.increment(TestProfiler.class,"testCallCounter");
+        Profile.increment(TestProfiler.class,"testCallCounter");
+        Profile.increment(TestProfiler.class,"testCallCounter");
+        assertEquals(3,Profile.metrics.get(TestProfiler.class.getName()).get("testCallCounter").get());
+    }
+    @Test
+    public void testReset(){
+        Profile.increment(TestProfiler.class,"testCallCounter");
+        Profile.reset();
+        assertTrue(Profile.metrics.isEmpty());
+    }
+
+    @Test
+    public void testToString(){
+        Profile.increment(TestProfiler.class,"testCallCounter");
+        Profile.increment(String.class,"someStringFn");
+        Profile.increment(String.class,"someStringFn");
+
+        String output = Profile.report();
+        System.out.println(output);
+    }
+}

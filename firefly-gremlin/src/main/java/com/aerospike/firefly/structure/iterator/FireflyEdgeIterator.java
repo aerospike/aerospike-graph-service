2c95048335a6806a7d1b6584261d6364f70652e2
Grant Haywood <grant@iowntheinter.net>
2022-06-16 17:50:33 -0600

    change access to public now that moved to seprate package

--- firefly-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyEdgeIterator.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyEdgeIterator.java
@@ -12,7 +12,7 @@ import java.util.Iterator;
  * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
  */
 public class FireflyEdgeIterator<T> extends FireflyElementIterator<Edge> {
-    protected FireflyEdgeIterator(FireflyGraph graph, Iterator<T> idIterator) {
+    public FireflyEdgeIterator(FireflyGraph graph, Iterator<T> idIterator) {
         super(graph.db, idIterator,
                 id -> graph.getBaseGraph().edgeExists(FireflyId.of(graph.db, FireflyEdge.class, id)),
                 id -> graph.getBaseGraph().readEdge(graph, FireflyId.of(graph.db,FireflyEdge.class, id)));
--- firefly-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyVertexIterator.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyVertexIterator.java
@@ -12,7 +12,7 @@ import java.util.Iterator;
  * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
  */
 public class FireflyVertexIterator<T> extends FireflyElementIterator<Vertex> {
-    protected FireflyVertexIterator(FireflyGraph graph, Iterator<T> idIterator) {
+    public FireflyVertexIterator(FireflyGraph graph, Iterator<T> idIterator) {
         super(graph.db, idIterator,
                 id -> graph.getBaseGraph().vertexExists(FireflyId.of(graph.db, FireflyVertex.class, id)),
                 id -> graph.getBaseGraph().readVertex(graph, FireflyId.of(graph.db,FireflyVertex.class, id)));

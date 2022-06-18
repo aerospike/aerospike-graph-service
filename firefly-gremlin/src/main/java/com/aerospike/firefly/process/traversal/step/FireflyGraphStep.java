fc1201fa082cdd37e6c7d8fb668ee7c61dfca678
Grant Haywood <grant@iowntheinter.net>
2022-06-17 23:56:31 -0600

    sketch FireflyGraphStep

---
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/FireflyGraphStep.java
@@ -0,0 +1,4 @@
+package com.aerospike.firefly.process.traversal.step;/**
+ * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
+ */public class FireflyGraphStep {
+}

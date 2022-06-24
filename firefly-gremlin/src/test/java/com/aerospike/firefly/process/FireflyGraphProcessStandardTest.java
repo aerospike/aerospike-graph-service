fbe94b55147527c29c4e9987a886938a99b16868
Lyndon Bauto <lyndonb@apache.org>
2022-06-24 15:45:41 -0700

    GRAPH-46 Removing some unnecessary code

--- firefly-gremlin/src/test/java/com/aerospike/firefly/process/FireflyGraphProcessStandardTest.java
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/process/FireflyGraphProcessStandardTest.java
@@ -1,38 +1,15 @@
 package com.aerospike.firefly.process;
 
-import com.aerospike.firefly.io.AerospikeConnection;
 import com.aerospike.firefly.structure.FireflyGraph;
 import com.aerospike.firefly.structure.FireflyGraphProvider;
-import com.aerospike.firefly.util.ConfigurationHelper;
-import org.apache.commons.configuration2.Configuration;
 import org.apache.tinkerpop.gremlin.GraphProviderClass;
 import org.apache.tinkerpop.gremlin.process.ProcessStandardSuite;
-import org.junit.After;
 import org.junit.runner.RunWith;
-
-import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES;
 
 /**
  * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
  */
-
-
 @RunWith(ProcessStandardSuite.class)
 @GraphProviderClass(provider = FireflyGraphProvider.class, graph = FireflyGraph.class)
 public class FireflyGraphProcessStandardTest {
-    private static final Configuration config;
-
-    static {
-        config = ConfigurationHelper.loadFromResources(INTEGRATION_TEST_PROPERTIES);
-    }
-
-    @After
-    public void closeGraphClearData() {
-        AerospikeConnection db = AerospikeConnection.connect(config);
-        db.dropDatabase();
-        db.close();
-    }
-
-
-
 }

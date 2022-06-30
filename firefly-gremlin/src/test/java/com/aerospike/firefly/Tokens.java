a5d58da950aee6a30506997f439e7512257b74f9
Grant Haywood <grant@iowntheinter.net>
2022-06-30 04:35:24 +0000

    add movielens test suite

--- firefly-gremlin/src/test/java/com/aerospike/firefly/Tokens.java
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/Tokens.java
@@ -9,4 +9,5 @@ public final class Tokens {
 
     public static final String INTEGRATION_TEST_PROPERTIES = "integration-test-settings.properties";
     public static final String AIR_ROUTES_50K_URL = "https://raw.githubusercontent.com/krlawrence/graph/master/sample-data/air-routes-latest.graphml";
+    public static final String MOVIELENS_1M_URL = "http://files.phaseshift.studio/movielens.kryo";
 }

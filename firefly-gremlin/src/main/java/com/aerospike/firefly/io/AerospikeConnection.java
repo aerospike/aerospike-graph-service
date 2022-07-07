2fd802b2ebc2a3e1f0f2038569dd18bed88f79c2
Lyndon Bauto <lyndonb@apache.org>
2022-07-07 13:53:14 -0700

    GRAPH-55 added a few more missing {}->%s String.format issues

--- firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
@@ -450,7 +450,7 @@ public class AerospikeConnection {
         if (String.class.isAssignableFrom(value.getClass())) {
             stmt.setFilter(Filter.contains(getElementPropertySet(FireflyEdge.class), IndexCollectionType.MAPVALUES, (String) value));
         } else {
-            throw new RuntimeException(String.format("{} not a string", value.getClass()));
+            throw new RuntimeException(String.format("%s not a string", value.getClass()));
         }
         final QueryPolicy p = new QueryPolicy();
         final RecordSet rs = client.query(p, stmt);
@@ -520,7 +520,7 @@ public class AerospikeConnection {
         if (String.class.isAssignableFrom(value.getClass())) {
             stmt.setFilter(Filter.contains(KEY_VALUE, IndexCollectionType.MAPVALUES, (String) value));
         } else {
-            throw new RuntimeException(String.format("{} not a string", value.getClass()));
+            throw new RuntimeException(String.format("%s not a string", value.getClass()));
         }
         final QueryPolicy p = new QueryPolicy();
         final RecordSet rs = client.query(p, stmt);

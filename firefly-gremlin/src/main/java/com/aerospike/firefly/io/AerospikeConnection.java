b3d35fcac0f98e1a0f37f7922adf789a308bff42
Lyndon Bauto <lyndonb@apache.org>
2022-07-12 16:24:44 -0700

    Removing unused parameter

--- firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
@@ -44,8 +44,6 @@ public class AerospikeConnection {
     final int NumLoops = 2;
     final int CommandsPerEventLoop = 50;
     final int DelayQueueSize = 50;
-    final int DelayPerSetExistsCheck = 100;
-    final int LoopsPerSetExistsCheck = 1000;
 
     final EventLoops eventLoops;
 

f54c6f8c8bee91845dec0192b7c1888c90417bad
lyndonbauto <lyndonb@apache.org>
2022-09-20 21:18:43 -0700

    GRAPH-131 exposed properties in docker container.

--- scripts/gremlin-server-docker.sh
+++ scripts/gremlin-server-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 # This is used by the Dockerfile. It is not intended to be run directly.
-python3 $CONF_DIR/firefly-graph-configure.py $CONF_DIR/firefly-graph.properties $AEROSPIKE_HOST
+
+python3 $CONF_DIR/firefly-graph-configure.py $CONF_DIR/firefly-graph.properties
 gremlin-server.sh $CONF_DIR/firefly-gremlin-server.yaml

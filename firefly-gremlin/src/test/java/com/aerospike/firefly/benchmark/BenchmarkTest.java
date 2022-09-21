3a1c3cdfb93cda3f1423c6baffef36c2500f49eb
lyndonbauto <lyndonb@apache.org>
2022-09-21 16:30:33 -0700

    GRAPH-57 attempting to integrate results with github actions

--- firefly-gremlin/src/test/java/com/aerospike/firefly/benchmark/BenchmarkTest.java
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/benchmark/BenchmarkTest.java
@@ -6,15 +6,15 @@ import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -22,6 +22,7 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
@@ -31,21 +32,23 @@ import org.openjdk.jmh.runner.options.TimeValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileWriter;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
-// TODO: We should tune these parameters to provide the benchmark
-//       with results we like - likely add warmup and adjust
-//       iterations.
-@BenchmarkMode(Mode.All)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 @Warmup(iterations = 0)
-@Measurement(iterations = 2, time = 2, timeUnit = TimeUnit.MINUTES)
+// TODO: Make this 30 seconds.
+@Measurement(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
 public class BenchmarkTest {
+    // Sample usage: mvn test -Dfirefly.host=172.17.0.3 -Ddocker.benchmark=1 -Dtest=BenchmarkTest -DfailIfNoTests=false --no-transfer-progress
+    // where
     private static final Logger LOG = LoggerFactory.getLogger(BenchmarkTest.class);
     private static final String HOST = BenchmarkTestUtils.getHost();
     private static final int PORT = 8182;
@@ -53,7 +56,22 @@ public class BenchmarkTest {
     private Cluster cluster = null;
     private GraphTraversalSource g = null;
     private static final Cluster.Builder BUILDER = Cluster.build().addContactPoint(HOST).port(PORT).enableSsl(false);
-    public String host;
+    private static final Map<String, String> testToTraversal = Map.ofEntries(
+            Map.entry("benchmark_g_V_hasxairport_code_DFWx", "g.V().has(\"airport\", \"code\", \"DFW\")"),
+            Map.entry("benchmark_g_V_hasxcode_DFWx", "g.V().has(\"code\", \"DFW\")"),
+            Map.entry("benchmark_g_V_hasxcode_DFWx_outE_count", "g.V().has(\"code\", \"DFW\").outE().count()"),
+            Map.entry("benchmark_g_V_hasxcode_SFOx_out_out_out_hasxcode_YVRx", "g.V().has(\"code\", \"SFO\").out().out().out().has(\"code\", \"YVR\")"),
+            Map.entry("benchmark_g_V_hasxcode_SFOx_out_out_project_byxunfold_countx_byxunfold_hasxcountry_USx_count", "g.V().\n" +
+                    "                  has(\"code\", \"SFO\").\n" +
+                    "                  out().out().\n" +
+                    "                  dedup().fold().\n" +
+                    "                  project(\"totalAirportCountFromSFO\", \"USAirportCountFromSFO\").\n" +
+                    "                  by(__.unfold().count()).\n" +
+                    "                  by(__.unfold().has(\"country\", \"US\").count())"),
+            Map.entry("benchmark_g_e_hasxdist_gtx4000x_inV_values_dedup", "g.E().has(\"dist\", P.gt(4000L)).inV().values(\"city\").dedup()"),
+            Map.entry("benchmark_g_V_hasxcode_LHRx_outxroutex_hasxcountry_USx_valuesxcodex", "g.V().has(\"code\", \"LHR\").out(\"route\").has(\"country\", \"US\").values(\"code\")"),
+            Map.entry("benchmark_g_V_hasLabelxairportx_count", "g.V().hasLabel(\"airport\").count()")
+    );
 
     // Run before the class, this will run before all the benchmarks
     // and load the graph.
@@ -126,7 +144,40 @@ public class BenchmarkTest {
                 .timeout(TimeValue.minutes(2)); // Timeout
         BenchmarkTestUtils.appendJmhOptionsBuilder(optBuilder);
         Options opt = optBuilder.build();
-        new Runner(opt).run();
+        Collection<RunResult> runResult = new Runner(opt).run();
+
+        // Format:
+        //[
+        //  {
+        //      "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+        //          "unit": "Percent",
+        //          "value": 50
+        //  },
+        //  {
+        //      "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+        //          "unit": "Megabytes",
+        //          "value": 100,
+        //          "range": "3",
+        //          "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+        //  }
+        //]
+
+        final JSONArray root = new JSONArray();
+        runResult.stream().forEach(result-> {
+            JSONObject obj = new JSONObject();
+            obj.put("name", testToTraversal.get(result.getPrimaryResult().getLabel()));
+            obj.put("unit", "Throughput (op/sec)");
+            obj.put("value", (1 / result.getPrimaryResult().getScore()));
+            obj.put("range", (1 / result.getPrimaryResult().getScoreError()));
+            obj.put("extra", result.getPrimaryResult().getStatistics());
+            root.put(obj);
+        });
+        try (FileWriter file = new FileWriter("target/jmh-result.json")) {
+            file.write(root.toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Benchmark
@@ -141,45 +192,45 @@ public class BenchmarkTest {
         blackhole.consume(vertices);
     }
 
-    @Benchmark
-    public void benchmark_g_V_hasxcode_DFWx_outE_count(final Blackhole blackhole) {
-        final long outECount = g.V().has("code", "DFW").outE().count().next();
-        blackhole.consume(outECount);
-    }
+   // @Benchmark
+   // public void benchmark_g_V_hasxcode_DFWx_outE_count(final Blackhole blackhole) {
+   //     final long outECount = g.V().has("code", "DFW").outE().count().next();
+   //     blackhole.consume(outECount);
+   // }
 
-    @Benchmark
-    public void benchmark_g_V_hasxcode_SFOx_out_out_out_hasxcode_YVRx(final Blackhole blackhole) {
-        final List<Vertex> vertices = g.V().has("code", "SFO").out().out().out().has("code", "YVR").toList();
-        blackhole.consume(vertices);
-    }
+  //  @Benchmark
+  //  public void benchmark_g_V_hasxcode_SFOx_out_out_out_hasxcode_YVRx(final Blackhole blackhole) {
+  //      final List<Vertex> vertices = g.V().has("code", "SFO").out().out().out().has("code", "YVR").toList();
+  //      blackhole.consume(vertices);
+  //  }
+  //  @Benchmark
+  //  public void benchmark_g_V_hasxcode_SFOx_out_out_project_byxunfold_countx_byxunfold_hasxcountry_USx_count(final Blackhole blackhole) {
+  //      final Map<String, Object> projectionMap = g.V().
+  //              has("code", "SFO").
+  //              out().out().
+  //              dedup().fold().
+  //              project("totalAirportCountFromSFO", "USAirportCountFromSFO").
+  //              by(__.unfold().count()).
+  //              by(__.unfold().has("country", "US").count()).next();
+  //      blackhole.consume(projectionMap);
+  //  }
+//
+  //  @Benchmark
+  //  public void benchmark_g_e_hasxdist_gtx4000x_inV_values_dedup(final Blackhole blackhole) {
+  //      final List<Object> cities = g.E().has("dist", P.gt(4000L)).inV().values("city").dedup().toList();
+  //      blackhole.consume(cities);
+  //  }
+//
 
-    @Benchmark
-    public void benchmark_g_V_hasxcode_SFOx_out_out_project_byxunfold_countx_byxunfold_hasxcountry_USx_count(final Blackhole blackhole) {
-        final Map<String, Object> projectionMap = g.V().
-                has("code", "SFO").
-                out().out().
-                dedup().fold().
-                project("totalAirportCountFromSFO", "USAirportCountFromSFO").
-                by(__.unfold().count()).
-                by(__.unfold().has("country", "US").count()).next();
-        blackhole.consume(projectionMap);
-    }
-
-    @Benchmark
-    public void benchmark_g_e_hasxdist_gtx4000x_inV_values_dedup(final Blackhole blackhole) {
-        final List<Object> cities = g.E().has("dist", P.gt(4000L)).inV().values("city").dedup().toList();
-        blackhole.consume(cities);
-    }
-
-    @Benchmark
-    public void benchmark_g_V_hasxcode_LHRx_outxroutex_hasxcountry_USx_valuesxcodex(final Blackhole blackhole) {
-        final List<Object> codes = g.V().has("code", "LHR").out("route").has("country", "US").values("code").toList();
-        blackhole.consume(codes);
-    }
-
-    @Benchmark
-    public void benchmark_g_V_hasLabelxairportx_count(final Blackhole blackhole) {
-        final long airportCount = g.V().hasLabel("airport").count().next();
-        blackhole.consume(airportCount);
-    }
+  //  @Benchmark
+  //  public void benchmark_g_V_hasxcode_LHRx_outxroutex_hasxcountry_USx_valuesxcodex(final Blackhole blackhole) {
+  //      final List<Object> codes = g.V().has("code", "LHR").out("route").has("country", "US").values("code").toList();
+  //      blackhole.consume(codes);
+  //  }
+//
+  //  @Benchmark
+  //  public void benchmark_g_V_hasLabelxairportx_count(final Blackhole blackhole) {
+  //      final long airportCount = g.V().hasLabel("airport").count().next();
+  //      blackhole.consume(airportCount);
+  //  }
 }

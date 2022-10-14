65c1312893fd310716782e47043d927bd173c56e
Lyndon Bauto <lyndonb@apache.org>
2022-10-14 13:54:35 -0700

    Changing error grammar

--- conf/docker-default/firefly-graph-configure.py
+++ conf/docker-default/firefly-graph-configure.py
@@ -6,7 +6,7 @@ default_aerospike_namespace = "test"
 default_firefly_data_model = "packed"
 
 if "AEROSPIKE_HOST" not in os.environ:
-    err = "Error: No AEROSPIKE_HOST is provided."
+    err = "Error: No AEROSPIKE_HOST was provided."
     print(err)
     raise Exception(err)
 aerospike_host = os.environ.get("AEROSPIKE_HOST")
@@ -16,7 +16,7 @@ firefly_data_model = os.environ.get("FIREFLY_DATA_MODEL", default_firefly_data_m
 
 # Environment variable could be set to empty string.
 if aerospike_host == "":
-    err = "Error: No AEROSPIKE_HOST is provided."
+    err = "Error: No AEROSPIKE_HOST was provided."
     print(err)
     raise Exception(err)
 if aerospike_port == "":

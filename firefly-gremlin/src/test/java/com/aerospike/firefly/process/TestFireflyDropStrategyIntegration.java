ee969580eb313f4d31bc888ef9e506c6bcff3da5
Grant Haywood <grant@iowntheinter.net>
2022-11-06 23:09:16 -0700

    restructure tests to match tinkerpop file layout

--- firefly-gremlin/src/test/java/com/aerospike/firefly/process/TestFireflyDropStrategyIntegration.java
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/process/TestFireflyDropStrategyIntegration.java
@@ -1,4 +1,4 @@
-package com.aerospike.firefly.structure.process;
+package com.aerospike.firefly.process;
 
 import com.aerospike.firefly.io.impl.relational.star.packed.StarPackedGraph;
 import com.aerospike.firefly.process.traversal.strategy.optimization.FireflyGraphDropStrategy;

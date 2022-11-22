2484fac525c0ce6a2ddeb4d627b251455c365e7c
Lyndon Bauto <lyndonb@apache.org>
2022-11-22 09:56:28 -0800

    GRAPH-121 Removing custom test from fireflygraph.

--- firefly-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
@@ -71,7 +71,6 @@ import static com.aerospike.firefly.util.Tokens.VERTEX_PROPERTY_ID_COUNTER;
 @Graph.OptIn(Graph.OptIn.SUITE_STRUCTURE_STANDARD)
 @Graph.OptIn(Graph.OptIn.SUITE_PROCESS_STANDARD)
 @Graph.OptIn("com.aerospike.firefly.structure.process.CustomGraphProcessStandardTest")
-@Graph.OptIn("com.aerospike.firefly.custom.CustomTestSuite")
 
 @Graph.OptOut(test = "org.apache.tinkerpop.gremlin.structure.TransactionTest", method = "*", reason = "MAKE ACTIVE WHEN TRANSACTIONS IMPLEMENTED", computers = {"ALL"})
 @Graph.OptOut(test = "org.apache.tinkerpop.gremlin.process.traversal.TraversalInterruptionTest", method = "*", reason = "MAKE ACTIVE WHEN PARALLEL SCAN RESULT ITERATOR IMPLEMENTED", computers = {"ALL"})

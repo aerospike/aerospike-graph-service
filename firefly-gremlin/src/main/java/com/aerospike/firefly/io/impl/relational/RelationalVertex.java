422d602e00ae3ed21caf8309c125cedb2bfa39c9
Grant Haywood <grant@iowntheinter.net>
2022-12-07 11:48:13 -0700

    remove System.out.println

--- firefly-gremlin/src/main/java/com/aerospike/firefly/io/impl/relational/RelationalVertex.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/io/impl/relational/RelationalVertex.java
@@ -258,8 +258,6 @@ public abstract class RelationalVertex extends FireflyVertex {
         if (this.isEdgeCacheDisabled) {
             final List<FireflyId> edgeIds = getEdgeIdsFromVertex(direction);
             vertices.addAll(verticesFromEdgeIds(edgeIds, direction, edgeLabels));
-            System.out.println(edgeIds);
-            System.out.println(vertices);
         } else {
             final List<FireflyId> vertexIds = new ArrayList<>();
             appendAdjacentVertexIds(vertexIds, direction, edgeLabels);

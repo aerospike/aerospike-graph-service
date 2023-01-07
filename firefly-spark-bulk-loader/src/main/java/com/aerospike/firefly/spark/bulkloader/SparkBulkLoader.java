a62e0301d27fc63d99c9aa2a364b32079dadd0a6
mbelsare <manishbelsare2003@gmail.com>
2023-01-06 19:11:25 -0800

    fixed based on comments

--- firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
+++ firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
@@ -228,14 +228,13 @@ public class SparkBulkLoader {
         }
 
         // Edges
-        // Get the first element of the list to use it for union in the loop
+        // Get the first DS in the list to use it for union in the loop
         Dataset<Row> unionDS = edgeDatasets.get(0);
         for (final Dataset<Row> edgeData : edgeDatasets) {
-            // union the temp DS with the next element
+            // union the temp DS with the next DS
             unionDS = unionDS.unionByName(edgeData, true).distinct();
         }
 
-        unionDS.show();
         final String finalS3BucketName = s3BucketName;
         final String finalConfigPath = configPath;
         // persist the union dataframe to allow for subsequent transformations to avoid calling old transformations again

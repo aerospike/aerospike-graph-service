a2dd1968c3c20cd13ac01d7e726b1e3bfe3cc8d9
Manish Belsare <manishbelsare2003@gmail.com>
2023-01-10 19:28:30 -0800

    Update firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
    Co-authored-by: Simon Z <14843662+DKZed@users.noreply.github.com>

--- firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
+++ firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
@@ -558,7 +558,7 @@ public class SparkBulkLoader {
     }
 
     static private void checkIfDirectoryEmpty(File directory) throws IOException {
-        if(Files.list(Paths.get(directory.getPath())).findAny().isEmpty()){
+        if (Files.list(Paths.get(directory.getPath())).findAny().isEmpty()) {
             throw new IOException("Empty directory found for path: " + directory);
         }
     }

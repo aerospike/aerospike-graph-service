aea65efd1a0a6dd39be6f525889be170dcd6c1f2
Lyndon Bauto <lyndonb@apache.org>
2023-02-08 14:24:05 -0800

    GRAPH-274 Added exception message checking.

--- firefly-gremlin/src/test/java/com/aerospike/firefly/security/lambda/ValidateLambdaCodeInjectionDisabledTest.java
+++ firefly-gremlin/src/test/java/com/aerospike/firefly/security/lambda/ValidateLambdaCodeInjectionDisabledTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -23,18 +24,22 @@ public class ValidateLambdaCodeInjectionDisabledTest extends AbstractFireflySuit
         GraphHelper.cloneElements(TinkerFactory.createModern(), graph);
         final GraphTraversalSource g = graph.traversal();
 
+        final String lambda1 = "it.get().label().equals('person')";
         try {
-            final List<Vertex> markos = g.V().has("name", "marko").filter(Lambda.predicate("it.get().label().equals('person')")).toList();
+            final List<Vertex> markos = g.V().has("name", "marko").filter(Lambda.predicate(lambda1)).toList();
             fail("Lambda code injection should be disabled.");
-        } catch (final Exception ignored) {
-            // Expect an exception.
+        } catch (final Exception e) {
+            // Expect an exception
+            assertEquals("The provided traversal contains a lambda step: LambdaFilterStep(lambda[" + lambda1 + "])", e.getMessage());
         }
 
+        final String lambda2 = "it.get().label().equals('person')";
         try {
-            final List<Object> markoOuts = g.V().has("name", "marko").flatMap(Lambda.function("it.get().vertices(Direction.OUT)", "gremlin-groovy")).toList();
+            final List<Object> markoOuts = g.V().has("name", "marko").flatMap(Lambda.function(lambda2, "gremlin-groovy")).toList();
             fail("Lambda code injection should be disabled.");
-        } catch (final Exception ignored) {
-            // Expect an exception.
+        } catch (final Exception e) {
+            // Expect an exception
+            assertEquals("The provided traversal contains a lambda step: LambdaFlatMapStep(lambda[" + lambda2 + "])", e.getMessage());
         }
 
     }

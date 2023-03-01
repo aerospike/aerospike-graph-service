301e5bdf556a018898ca18d88a50d54d44e6f864
Manish Belsare <mbelsare@aerospike.com>
2023-03-01 15:58:53 -0800

    Update firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
    Co-authored-by: Lyndon Bauto <lyndonb@apache.org>

--- firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
+++ firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/SparkBulkLoader.java
@@ -624,7 +624,7 @@ public class SparkBulkLoader {
     }
 
     static private void flushEdgeMap(final FireflyGraph graph, final Direction direction,
-                                     final ConcurrentHashMap<Long, ConcurrentHashMap<String, List<Value>>> edgeMap,
+                                     final Map<Long, Map<String, List<Value>>> edgeMap,
                                      final boolean ignoreElementCreationFailed) {
         for (Map.Entry<Long, Map<String, List<Value>>> vertexIdToLabelMaps : edgeMap.entrySet()) {
             final long vertexId = vertexIdToLabelMaps.getKey();

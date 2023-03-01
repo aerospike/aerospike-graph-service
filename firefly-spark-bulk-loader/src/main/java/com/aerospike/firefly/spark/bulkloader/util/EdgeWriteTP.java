f00ff1daffe3f577e177e62e2f4814b61281edb3
Manish Belsare <mbelsare@aerospike.com>
2023-03-01 15:59:12 -0800

    Update firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/util/EdgeWriteTP.java
    Co-authored-by: Lyndon Bauto <lyndonb@apache.org>

--- firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/util/EdgeWriteTP.java
+++ firefly-spark-bulk-loader/src/main/java/com/aerospike/firefly/spark/bulkloader/util/EdgeWriteTP.java
@@ -37,7 +37,7 @@ public class EdgeWriteTP implements Runnable {
     Map<Long, Map<String, List<Value>>> vertexInEdgeMap;
     GenericRowWithSchema row;
     private static final int RETRY_LIMIT = 100;
-    public EdgeWriteTP(Set<Long> supernodes, boolean ignoreFailedProperties, boolean useProvidedId, boolean keepProvidedId, String providedIdPropertyName, boolean ignoreElementCreationFailed, String nullValue, FireflyGraph graph, AtomicInteger outEdgeCount, AtomicInteger inEdgeCount, ConcurrentHashMap<Long, ConcurrentHashMap<String, List<Value>>> vertexOutEdgeMap, ConcurrentHashMap<Long, ConcurrentHashMap<String, List<Value>>> vertexInEdgeMap, GenericRowWithSchema row) {
+    public EdgeWriteTP(Set<Long> supernodes, boolean ignoreFailedProperties, boolean useProvidedId, boolean keepProvidedId, String providedIdPropertyName, boolean ignoreElementCreationFailed, String nullValue, FireflyGraph graph, AtomicInteger outEdgeCount, AtomicInteger inEdgeCount, Map<Long, Map<String, List<Value>>> vertexOutEdgeMap, Map<Long, Map<String, List<Value>>> vertexInEdgeMap, GenericRowWithSchema row) {
         this.supernodes = supernodes;
         this.ignoreFailedProperties = ignoreFailedProperties;
         this.useProvidedId = useProvidedId;

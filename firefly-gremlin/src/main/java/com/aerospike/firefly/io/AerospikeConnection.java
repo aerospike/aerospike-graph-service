c9b52efa076a87a21cfe2c5ebd28f198241a40a8
Lyndon Bauto <lyndonb@apache.org>
2023-03-03 10:52:23 -0800

    GRAPH-399 Fixed has container issues.

--- firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
@@ -836,7 +836,7 @@ public class AerospikeConnection implements AutoCloseable {
         final FireflyCache cache = transactionCache.get();
         final Record[] results;
         try { //@todo policy causes key mismatch error
-            results = (cache != null) ? cache.read(new Key[]{key}) : new Record[]{client.get(policy, key)};
+            results = (cache != null) ? new Record[]{cache.read(key)} : new Record[]{client.get(policy, key)};
         } catch (final AerospikeException e) {
             LOG.error("Error: AerospikeException in read {}", e.getMessage());
             throw e;
@@ -867,7 +867,7 @@ public class AerospikeConnection implements AutoCloseable {
         final FireflyCache cache = transactionCache.get();
         final Record[] results;
         try { //@todo policy causes key mismatch error
-            results = (cache != null) ? cache.read(keys) : client.get(batchPolicy, keys);
+            results = (cache != null) ? cache.read(keys, batchPolicy) : client.get(batchPolicy, keys);
         } catch (final AerospikeException e) {
             LOG.error("Error: AerospikeException in read {}", e.getMessage());
             throw e;

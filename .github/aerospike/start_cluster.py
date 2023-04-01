5c160b2e404641d20b456de108768a0e7e17644c
Grant Haywood <grant@iowntheinter.net>
2023-03-31 18:14:59 -0600

    mac compatibility

--- .github/aerospike/start_cluster.py
+++ .github/aerospike/start_cluster.py
@@ -11,7 +11,6 @@ from docker.client import DockerClient
 def parse_cli():
     parser = argparse.ArgumentParser(description="run aerospike cluster")
     parser.add_argument('--aerospike_version', type=str, help="version of aerospike")
-    parser.add_argument('--aerospike_conf', type=str, help="aerospike configuration location on host machine")
     parser.add_argument('--features_file', type=str, help="base64 encoded features file")
     parser.add_argument('--repo_path', type=str, help="repo path")
     parser.add_argument('--debug', help="debug logging", action="store_true", default=False)
@@ -20,15 +19,14 @@ def parse_cli():
     cli = parser.parse_args()
     assert cli.repo_path is not None
     assert cli.aerospike_version is not None
-    assert cli.aerospike_conf is not None
     assert cli.features_file is not None
 
     return Box({"aerospike_version": cli.aerospike_version,
-                "aerospike_conf": cli.aerospike_conf,
                 "features_file": cli.features_file,
                 "repo_path": cli.repo_path,
                 "aerospike_image": f"aerospike:{cli.aerospike_version}",
-                "debug": cli.debug})
+                "debug": cli.debug,
+                "test": cli.test if "test" in cli else False})
 
 
 def get_mounts(config) -> List[Dict]:
@@ -46,14 +44,11 @@ def start_aerospike_node(incr: int, config: Box, docker_client: DockerClient):
     mounts = get_mounts(config)
 
     it = docker_client.containers.run(config.aerospike_image,
-                                      command=" --config-file /opt/aerospike/etc/aerospike.conf",
+                                      command=f" --config-file /opt/aerospike/etc/aerospike_{incr}.conf",
                                       environment={"FEATURE_KEY_FILE": "/opt/aerospike/etc/features.conf",
                                                    "MEM_GB": 2},
-                                      ports={3000: f'30{incr}0/tcp',
-                                             3001: f'30{incr}1/tcp',
-                                             3002: f'30{incr}2/tcp',
-                                             3003: f'30{incr}3/tcp',
-                                             4333: f'43{incr}3/tcp'},
+                                      ports={f'30{incr}0/tcp': f'30{incr}0/tcp',
+                                             f'43{incr}3/tcp': f'43{incr}3/tcp'},
                                       detach=True,
                                       mounts=mounts)
     logger.info(f"started aerospike container {it.id}")
@@ -69,8 +64,8 @@ def run_asinfo_cmd(cmd: str, ctr_id: str, docker_client: DockerClient):
     return exit, output
 
 
-def run_asinfo_tip(ctr_id: str, peer_ip: str, docker_client: DockerClient):
-    return run_asinfo_cmd(f"asinfo -v tip:host={peer_ip};port=3002", ctr_id, docker_client)
+def run_asinfo_tip(incr: int, ctr_id: str, peer_ip: str, docker_client: DockerClient):
+    return run_asinfo_cmd(f"asinfo -p 30{incr}0 -v tip:host={peer_ip};port=3002", ctr_id, docker_client)
 
 
 def get_ctr_ip(ctr_id: str, docker_client: DockerClient) -> str:
@@ -80,8 +75,9 @@ def get_ctr_ip(ctr_id: str, docker_client: DockerClient) -> str:
     return x
 
 
-def healthcheck(ctr_id: str, docker_client: DockerClient) -> bool:
-    cmd = "asinfo -v cluster-stable:size=3;ignore-migrations=true;namespace=test"
+def healthcheck(incr, ctr_id: str, docker_client: DockerClient) -> bool:
+    logger.info(f"will healthcheck {ctr_id} with incr {incr}")
+    cmd = f"asinfo -p 30{incr}0 -v cluster-stable:size=3;ignore-migrations=true;namespace=test"
     exit, output = run_asinfo_cmd(cmd, ctr_id, docker_client)
     logger.info(f"healthcheck: {output}")
     return exit == 0
@@ -94,21 +90,21 @@ def shutdown(ctr_id: str, docker_client: DockerClient):
 
 
 def start_aerospike_cluster(config, docker_client):
+    ctr_id_0: str = start_aerospike_node(0, config, docker_client)
+    logger.info(f"started aerospike container {ctr_id_0}")
     ctr_id_1: str = start_aerospike_node(1, config, docker_client)
     logger.info(f"started aerospike container {ctr_id_1}")
     ctr_id_2: str = start_aerospike_node(2, config, docker_client)
     logger.info(f"started aerospike container {ctr_id_2}")
-    ctr_id_3: str = start_aerospike_node(3, config, docker_client)
-    logger.info(f"started aerospike container {ctr_id_3}")
 
     # tip container 2 to peer with container 1
-    logger.info(f"tip container {ctr_id_2} to peer with container {ctr_id_1}")
-    run_asinfo_tip(ctr_id_2, get_ctr_ip(ctr_id_1, docker_client), docker_client)
+    logger.info(f"tip container {ctr_id_1} to peer with container {ctr_id_0}")
+    run_asinfo_tip(1, ctr_id_1, get_ctr_ip(ctr_id_0, docker_client), docker_client)
     # tip container 3 to peer with container 1
-    logger.info(f"tip container {ctr_id_3} to peer with container {ctr_id_1}")
-    run_asinfo_tip(ctr_id_3, get_ctr_ip(ctr_id_1, docker_client), docker_client)
+    logger.info(f"tip container {ctr_id_2} to peer with container {ctr_id_0}")
+    run_asinfo_tip(2, ctr_id_2, get_ctr_ip(ctr_id_0, docker_client), docker_client)
 
-    return [ctr_id_1, ctr_id_2, ctr_id_3]
+    return [ctr_id_0, ctr_id_1, ctr_id_2]
 
 
 if __name__ == '__main__':
@@ -119,8 +115,8 @@ if __name__ == '__main__':
         logger.basicConfig(level=logger.DEBUG)
     nodes: List = start_aerospike_cluster(config, docker_client)
     time.sleep(2)
-    for node in nodes:
-        assert healthcheck(node, docker_client)
-    if config.test:
+    for i in range(0, 3):
+        assert healthcheck(i, nodes[i], docker_client)
+    if "test" in config and config.test:
         for node in nodes:
             shutdown(node, docker_client)

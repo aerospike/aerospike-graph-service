58b4f91ed4a4f0bf140eb0434188741ed107566b
Grant Haywood <grant@iowntheinter.net>
2023-04-03 17:57:22 -0600

    add debug logging

--- firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
+++ firefly-gremlin/src/main/java/com/aerospike/firefly/io/AerospikeConnection.java
@@ -1688,8 +1688,9 @@ public class AerospikeConnection implements AutoCloseable {
         } catch (final AerospikeException ae) {
             switch (ae.getResultCode()) {
                 case ResultCode.RECORD_TOO_BIG:
+                    LOG.error("RECORD_TO_BIG error on key {}", key);
                     LOG.error(RECORD_TOO_BIG, ae);
-                    throw new RuntimeException(RECORD_TOO_BIG, ae);
+                    throw ae;
                 case ResultCode.KEY_NOT_FOUND_ERROR:
                     LOG.debug(ELEMENT_NOT_FOUND, ae);
                     throw new ElementNotFoundException(ae);

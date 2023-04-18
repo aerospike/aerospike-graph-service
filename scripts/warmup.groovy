8414b90846421b987abeccbcfa32cdc1a6c617bb
Grant Haywood <grant@iowntheinter.net>
2023-04-18 00:18:49 -0600

    quiet error message

--- scripts/warmup.groovy
+++ scripts/warmup.groovy
@@ -1,8 +1,11 @@
 import java.util.stream.IntStream
 
-g = AnonymousTraversalSource.traversal().withRemote(DriverRemoteConnection.using("localhost", 8182, "g"));
-IntStream.range(0, 48).forEach { it ->
-    g.V("FIREFLY_WARMUP").next()
+try {
+    g = AnonymousTraversalSource.traversal().withRemote(DriverRemoteConnection.using("localhost", 8182, "g"));
+    IntStream.range(0, 48).forEach { it ->
+        g.V("FIREFLY_WARMUP").next()
+    }
+} catch (e) {
+    println "Failed to perform warmup routine: ${e.getMessage()}"
 }
-
 

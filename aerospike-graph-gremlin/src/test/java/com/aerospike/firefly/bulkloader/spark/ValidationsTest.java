ba4552c23f823ea20b39223c3d8a7d14cb9fe6f0
Rahul Kumar <58450859+rahul-aerospike@users.noreply.github.com>
2023-06-01 11:07:31 -0700

    Update aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/bulkloader/spark/ValidationsTest.java
    Co-authored-by: Simon Z <14843662+DKZed@users.noreply.github.com>

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/bulkloader/spark/ValidationsTest.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/bulkloader/spark/ValidationsTest.java
@@ -13,8 +13,8 @@ import org.apache.spark.sql.types.StructType;
 import java.util.Arrays;
 
 public class ValidationsTest extends TestCase {
-
     private SparkSession spark;
+    
     public void setUp() throws Exception {
         spark = SparkSession
                 .builder()

23e78aabaa20dca3846e759177fa5bd7492df6b2
Grant Haywood <grant@iowntheinter.net>
2023-06-21 01:10:40 -0600

    update docker build process to squash image, remove arm support for the moment

---
+++ scripts/build-docker.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+OUTPUT_TAG="$1"
+if [[ -z "$OUTPUT_TAG" ]]; then
+    echo "Usage: $0 <tag> [platform]"
+    exit 1
+fi
+
+PLATFORM="$2"
+if [[ -z "$PLATFORM" ]]; then
+    PLATFORM="linux/amd64"
+fi
+
+BUILD_IMAGE="aerospike-graph-build:latest"
+SQUASH_IMAGE="aerospike-graph-squash:latest"
+
+#do the initial build
+docker build $EXTRA_BUILD_ARGS --platform "$PLATFORM" --tag $BUILD_IMAGE -f docker/build.Dockerfile .
+
+#instantiate container and get container id
+CTR_ID=$(docker run -d -t -i --entrypoint=/bin/echo $BUILD_IMAGE)
+
+#export container filesystem to new image stripping historic layers
+NEW_IMAGE=$(docker export "$CTR_ID" | docker import -)
+docker tag "$NEW_IMAGE" "$SQUASH_IMAGE"
+
+#add the runtime configuration to the stripped image
+docker build $EXTRA_BUILD_ARGS -f docker/prod.Dockerfile --tag "$OUTPUT_TAG" .

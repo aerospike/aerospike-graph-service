c9d57de1297c6323c5598af2b62a97c3a6b3e25f
lyndon <lyndonb@apache.org>
2023-08-01 11:49:59 -0700

    GRAPH-565 Added ~supernode flag

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/impl/relational/RelationalVertex.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/impl/relational/RelationalVertex.java
@@ -488,12 +488,10 @@ public abstract class RelationalVertex extends FireflyVertex {
     @Override
     public void setCacheDisabled() {
         final Key key = getKey(db, this.db.VERTEX_AERO_SET, this.id);
-        final Operation updateCacheState = ExpOperation.write(this.db.EDGE_CACHE_DISABLED_BIN, Exp.build(Exp.val(true)), ExpWriteFlags.DEFAULT);
-
-        // Operate on database.
         final WritePolicy writePolicy = new WritePolicy();
         writePolicy.recordExistsAction = RecordExistsAction.UPDATE_ONLY;
-        this.db.operate(writePolicy, key, updateCacheState);
+        this.db.operate(writePolicy, key,
+                Operation.put(new Bin(this.db.EDGE_CACHE_DISABLED_BIN, Value.get(true))));
         this.isEdgeCacheOverflowed = true;
     }
 

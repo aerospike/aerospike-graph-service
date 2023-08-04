08ea3df405d2c213cdaa199b049ddf7ac3b23731
Grant Haywood <grant@iowntheinter.net>
2023-08-03 19:41:00 -0600

    Update aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/PluginUtil.java
    Co-authored-by: Simon Z <14843662+DKZed@users.noreply.github.com>

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/PluginUtil.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/PluginUtil.java
@@ -16,9 +16,9 @@ public class PluginUtil {
             final Class clazz = Optional.ofNullable(Class.forName(className)).orElseThrow(() ->
                     new RuntimeException("Could not load class: " + className));
             return clazz.getMethod(STATIC_OPEN_METHOD, Configuration.class).invoke(null, config);
-        } catch (NoSuchMethodException nsm) {
+        } catch (final NoSuchMethodException nsm) {
             throw new RuntimeException(String.format("Class %s does not properly implement a static open(Configuration) method", className));
-        } catch (Exception e) {
+        } catch (final Exception e) {
             throw new RuntimeException(e);
         }
     }

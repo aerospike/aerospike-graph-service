f180089dd09c4047558c3d65e6c32d3526bc3600
lyndon <lyndonb@apache.org>
2023-08-08 12:11:22 -0700

    GRAPH-673 Working through test failures.

--- aerospike-graph-bulk-loader/src/test/java/com/aerospike/firefly/bulkloader/integration/Tokens.java
+++ aerospike-graph-bulk-loader/src/test/java/com/aerospike/firefly/bulkloader/integration/Tokens.java
@@ -18,7 +18,6 @@ public final class Tokens {
     private static final Map<String, Path> INTEGRATION_TEST_CONFIGURATIONS = ImmutableMap.of(
             PackedGraph.DATA_MODEL, Path.of("../conf/integration-test-settings-packed.properties"),
             PackedGraph.DATA_MODEL + "-sindex", Path.of("../conf/integration-test-settings-packed-sindex.properties"),
-            StarPackedGraph.DATA_MODEL, Path.of("../conf/integration-test-settings-star-packed.properties")
     );
 
     public static final Path INTEGRATION_TEST_PROPERTIES;

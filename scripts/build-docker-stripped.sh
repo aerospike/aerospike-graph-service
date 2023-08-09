1ba5c38ba3bf193e7c282b767649cf66e709126b
lyndon <lyndonb@apache.org>
2023-08-09 11:08:52 -0700

    GRAPH-673 Switching docker spark image to 3.3.0 and fixing pom/dockerfiles up for cr

--- scripts/build-docker-stripped.sh
+++ scripts/build-docker-stripped.sh
@@ -4,8 +4,6 @@ set -o pipefail
 DOCKERFILE_A=${DOCKERFILE_A:-"docker/build-stripped.Dockerfile"}
 DOCKERFILE_B=${DOCKERFILE_B:-"docker/prod-stripped.Dockerfile"}
 
-
-
 OUTPUT_TAG="$1"
 if [[ -z "$OUTPUT_TAG" ]]; then
     echo "Usage: $0 <tag> [platform] [--push]"

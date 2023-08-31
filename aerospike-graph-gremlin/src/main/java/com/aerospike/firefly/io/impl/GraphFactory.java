86737a2ae5f3ee57cea17e8d28dcc6dd71e3b21d
Grant Haywood <grant@iowntheinter.net>
2023-08-31 13:56:25 -0600

    remove use of reflection in GraphFactory, simply implementation and removed unused upgrade codepath

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/impl/GraphFactory.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/impl/GraphFactory.java
@@ -20,28 +20,19 @@ import static com.aerospike.firefly.util.ConfigurationHelper.Keys.FIREFLY_DATA_M
  * @author Lyndon Bauto (<a href="https://github.com/lyndonbauto">https://github.com/lyndonbauto</a>)
  */
 final public class GraphFactory {
-    // We can map the class here, but we do not instantiate the Graphs because that would
-    // be unnecessary overhead.
     private static final Map<String, Class<? extends FireflyGraph>> DATA_MODEL_MAP = ImmutableMap.of(
             PackedGraph.DATA_MODEL, PackedGraph.class
     );
     private static final Logger LOG = LoggerFactory.getLogger(GraphFactory.class);
-
     public static FireflyGraph createGraph(final AerospikeConnection db, final Configuration config) {
-
         final String dataModel = ConfigurationHelper.getOrDefaultString(FIREFLY_DATA_MODEL, config);
         if (!DATA_MODEL_MAP.containsKey(dataModel)) {
             throw new IllegalArgumentException("Unknown graph type: " + dataModel);
         } else {
             LOG.info("Constructing Graph for {} data model.", dataModel);
             try {
-                final Class<? extends FireflyGraph> graphClass = DATA_MODEL_MAP.get(dataModel);
-                if (Upgrade.checkNeedsUpgrade(graphClass, db))
-                    Upgrade.performUpgrade(graphClass, db);
-                return graphClass.getConstructor(AerospikeConnection.class, Configuration.class, Settings.class).newInstance(db, config, getGremlinServerSettings());
+                return new PackedGraph(db, config, getGremlinServerSettings());
             } catch (Exception e) {
-                // This should never happen, but this prevents us from having to put a throws on the function signature.
-                // Gotta love Java...
                 throw new RuntimeException("Error constructing graph", e);
             }
         }

b4dd34805db7b1b84ee6620e4304c15769e47d70
lyndon <lyndonb@apache.org>
2023-09-12 09:54:33 -0700

    GRAPH-785 Trying github action to kill clusters and test for phantom edges

---
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/phantom_edges/TestPhantomEdges.java
@@ -0,0 +1,37 @@
+package com.aerospike.firefly.phantom_edges;
+
+
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.junit.Test;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
+/**
+ * @author Lyndon Bauto (<a href="https://github.com/lyndonbauto">https://github.com/lyndonbauto</a>)
+ */
+public class TestPhantomEdges {
+
+    // This test expects that an aerospike-graph docker instance is running in GCP and a bulk load has completed
+    // where one or more of the works was killed mid job. This will leave phantom edges in the graph which we will find.
+
+    @Test
+    public void findAllEdges() throws Exception {
+        final DriverRemoteConnection driverRemoteConnection = DriverRemoteConnection.using("localhost", 8182, "g");
+        final GraphTraversalSource g = traversal().withRemote(driverRemoteConnection);
+        System.out.println("Starting calculations.");
+        final long outECount = g.with("evaluationTimeout", 30 * 60 * 1000).V().outE().count().next();
+        System.out.println("outECount: " + outECount);
+        final long inECount = g.with("evaluationTimeout", 30 * 60 * 1000).V().inE().count().next();
+        System.out.println("inECount: " + inECount);
+        final long outCount = g.with("evaluationTimeout", 30 * 60 * 1000).V().out().count().next();
+        System.out.println("outCount: " + outCount);
+        final long inCount = g.with("evaluationTimeout", 30 * 60 * 1000).V().in().count().next();
+        System.out.println("inCount: " + inCount);
+        final long eCount = g.with("evaluationTimeout", 30 * 60 * 1000).E().count().next();
+        System.out.println("eCount: " + eCount);
+        final long vCount = g.with("evaluationTimeout", 30 * 60 * 1000).V().count().next();
+        System.out.println("vCount: " + vCount);
+        driverRemoteConnection.close();
+    }
+}

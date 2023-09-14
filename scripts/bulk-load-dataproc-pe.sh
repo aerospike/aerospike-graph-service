3fe8f01e63e22b4aefc4c6451b77fa99dbda6dc6
lyndon <lyndonb@apache.org>
2023-09-14 12:23:04 -0700

    GRAPH-785 fixing name

--- scripts/bulk-load-dataproc-pe.sh
+++ scripts/bulk-load-dataproc-pe.sh
@@ -86,7 +86,7 @@ echo "###################################"
 l3_start=`date +%s`
 
 { gcloud dataproc jobs submit spark  --class=com.aerospike.firefly.bulkloader.SparkBulkLoader --jars=${bulk_jar_uri} --id ${name}-job --cluster=${name}  --region=us-central1 -- -c ${properties_file_uri} -dryrun -writevertex -verifyvertex -writeedge -verifyedge; } &
-{ echo "sleeping 9 minutes"; sleep 9m; echo "Killing ci-pe-w-3"; gcloud compute instances delete ci-pe-${name}-w-3 --zone=us-central1-a --quiet; echo "Killing ci-pe-w-4"; gcloud compute instances delete ci-pe-${name}-w-4 --zone=us-central1-a --quiet; } &
+{ echo "sleeping 9 minutes"; sleep 9m; echo "Killing ci-pe-w-3"; gcloud compute instances delete ${name}-w-3 --zone=us-central1-a --quiet; echo "Killing ci-pe-w-4"; gcloud compute instances delete ${name}-w-4 --zone=us-central1-a --quiet; } &
 
 wait
 

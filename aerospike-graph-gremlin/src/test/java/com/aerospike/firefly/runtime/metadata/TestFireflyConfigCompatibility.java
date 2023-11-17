ab425e421037ea47c3da8441903a6b12e1962c92
Grant Haywood <grant@iowntheinter.net>
2023-11-17 14:22:03 -0700

    refactor structure of test suite

---
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/runtime/metadata/TestFireflyConfigCompatibility.java
@@ -0,0 +1,97 @@
+package com.aerospike.firefly.runtime.metadata;
+
+import com.aerospike.firefly.structure.FireflyGraph;
+import com.aerospike.firefly.util.ConfigurationHelper;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.ADJACENCY_INDEX_ENABLED_FLAG;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.DEBUG_MODE_FLAG;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.FIREFLY_DATA_MODEL;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.PHAT_EDGE_SIZE;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.SUMMARY_ENABLED_FLAG;
+
+public class TestFireflyConfigCompatibility {
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Before
+    public void beforeEach() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            graph.getBaseGraph().dropDatabase(graph, false);
+        }
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            final GraphTraversalSource g = graph.traversal();
+            final Vertex first = g.addV("first").next();
+            final Vertex second = g.addV("second").next();
+            g.addE("edge").from(first).to(second).iterate();
+        }
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            graph.getBaseGraph().dropDatabase(graph, true);
+        }
+    }
+
+    @Test
+    public void testToggleMutableConfig() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        config.setProperty(ADJACENCY_INDEX_ENABLED_FLAG, "false");
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            final GraphTraversalSource g = graph.traversal();
+            Assert.assertEquals(1, (long) g.V().hasLabel("first").out().count().next());
+        }
+    }
+
+    @Test
+    public void testImmutablePhatEdgeSize() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        exit.expectSystemExitWithStatus(1);
+        config.setProperty(PHAT_EDGE_SIZE, "1337");
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            Assert.fail("Graph should not start with immutable config change on key: " + PHAT_EDGE_SIZE);
+        }
+    }
+
+    @Test
+    public void testImmutableSummaryEnabled() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        exit.expectSystemExitWithStatus(1);
+        config.setProperty(SUMMARY_ENABLED_FLAG, "false");
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            Assert.fail("Graph should not start with immutable config change on key: " + SUMMARY_ENABLED_FLAG);
+        }
+    }
+
+    @Test
+    public void testImmutableDataModel() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        exit.expectSystemExitWithStatus(1);
+        config.setProperty(FIREFLY_DATA_MODEL, "the-cooler-packed");
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            Assert.fail("Graph should not start with immutable config change on key: " + FIREFLY_DATA_MODEL);
+        }
+    }
+
+    @Test
+    public void testImmutableDebugMode() {
+        final Configuration config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        exit.expectSystemExitWithStatus(1);
+        config.setProperty(DEBUG_MODE_FLAG.toLowerCase(), "true");
+        try (final FireflyGraph graph = FireflyGraph.open(config)) {
+            Assert.fail("Graph should not start with immutable config change on key: " + DEBUG_MODE_FLAG);
+        }
+    }
+}
---
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/runtime/server/TestFireflyServer.java
@@ -0,0 +1,149 @@
+package com.aerospike.firefly.runtime.server;
+
+import com.aerospike.firefly.runtime.FireflyServer;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
+/**
+ * @author Lyndon Bauto (<a href="https://github.com/lyndonbauto">https://github.com/lyndonbauto</a>)
+ */
+public class TestFireflyServer {
+
+    private static final long SECOND = 1000;
+    private static final long MINUTE = 60 * SECOND;
+    private static final long TEST_DURATION = 10 * MINUTE;
+    private long totalExecuted = 0;
+    private FireflyServer server;
+    private final boolean failed = false;
+    private final List<Long> heapSizes = new ArrayList<>();
+
+    @Test
+    public void testServer() throws Exception {
+        server = FireflyServer.main(new String[] {"../conf/firefly-gremlin-server-local.yaml"});
+        HEAP_REPORTING_TIMER.schedule(new HeapReport(), 0, 5000);
+        runManyQueries();
+        uploadHeapUsage();
+    }
+
+    private static final Timer HEAP_REPORTING_TIMER = new Timer(true);
+
+    private class HeapReport extends TimerTask {
+        @Override
+        public void run() {
+            System.gc();
+            System.gc();
+            System.gc();
+            heapSizes.add(Runtime.getRuntime().totalMemory() / 1024 / 1024);
+        }
+    }
+
+    public void runManyQueries() throws Exception {
+        final long startTime = System.currentTimeMillis();
+        final Cluster cluster = Cluster.build().addContactPoint("localhost").port(8182).create();
+        final DriverRemoteConnection drc = DriverRemoteConnection.using(cluster, "g");
+        final GraphTraversalSource g = traversal().withRemote(drc);
+        final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        g.V().drop().iterate();
+        for (int i = 0; i < 25000; i++) {
+            g.addV("randomVertex").
+                    property("last_seen", String.format("%d", System.currentTimeMillis())).
+                    property("something", String.format("%d", i)).
+                    property("otherThing", String.format("%d", i)).
+                    property("anotherThing", String.format("%d", i)).
+                    property("yetAnotherThing", String.format("%d", i)).
+                    property("andAnotherThing", String.format("%d", i)).
+                    property("andYetAnotherThing", String.format("%d", i)).
+                    iterate();
+        }
+
+        final List<Object> ids = g.V().id().toList();
+        final List<Future<?>> futures = new ArrayList<>();
+        while (System.currentTimeMillis() - startTime <= TEST_DURATION) {
+            while (futures.size() > 500) {
+                Thread.sleep(100);
+                final List<Future<?>> completedFutures = futures.stream().filter(Future::isDone).collect(Collectors.toList());
+                completedFutures.forEach(f -> {
+                    try {
+                        f.get();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                totalExecuted += completedFutures.size();
+                futures.removeAll(completedFutures);
+            }
+            final Random rand = new Random();
+            final List<Object> idz = new ArrayList<>();
+            for (int i = 0; i < 9; i++) {
+                idz.add(ids.get(rand.nextInt(ids.size())));
+            }
+            if (rand.nextBoolean()) {
+                futures.add(executorService.submit(() -> g.V(idz).properties("last_seen").toList()));
+            } else {
+                futures.add(executorService.submit(() -> g.V(idz).property("last_seen", System.currentTimeMillis()).iterate()));
+            }
+        }
+        futures.forEach(f-> {
+            try {
+                f.get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        drc.close();
+        Thread.sleep(30000);
+    }
+
+    void uploadHeapUsage() {
+        final List<Long> trimmedHeap = heapSizes.subList(heapSizes.size() - 20, heapSizes.size());
+
+        // Allow 50 MB swings in heap before we investigate deeper.
+        final Long averageHeap = trimmedHeap.stream().mapToLong(Long::longValue).sum() / trimmedHeap.size();
+        final Long maxHeap = trimmedHeap.stream().mapToLong(Long::longValue).max().getAsLong();
+        final Long minHeap = trimmedHeap.stream().mapToLong(Long::longValue).min().getAsLong();
+
+
+        final JSONArray root = new JSONArray();
+        JSONObject obj = new JSONObject();
+        obj.put("name", "Heap Usage Average");
+        obj.put("unit", "MB");
+        obj.put("value", averageHeap);
+        obj.put("range", maxHeap - minHeap);
+        root.put(obj);
+        obj = new JSONObject();
+        obj.put("name", "Heap Usage Max");
+        obj.put("unit", "MB");
+        obj.put("value", maxHeap);
+        obj.put("range", maxHeap - minHeap);
+        root.put(obj);
+        obj = new JSONObject();
+        obj.put("name", "Heap Usage Min");
+        obj.put("unit", "MB");
+        obj.put("value", minHeap);
+        obj.put("range", maxHeap - minHeap);
+        root.put(obj);
+        try (final FileWriter file = new FileWriter("target/jmh-result.json")) {
+            file.write(root.toString());
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

699b78f58af671325487aa66be0659dd39100a24
Simon Z <14843662+DKZed@users.noreply.github.com>
2024-02-12 12:01:54 -0800

    Update aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/aerospike/pagination/PageFetcher.java

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/aerospike/pagination/PageFetcher.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/aerospike/pagination/PageFetcher.java
@@ -46,7 +46,7 @@ public abstract class PageFetcher<E> {
                     if (readLoopExecutorService.isShutdown()) {
                         try {
                             pageQueue.put(new PoisonPill());
-                        } catch (finally InterruptedException e) {
+                        } catch (final InterruptedException e) {
                             LOG.error("Error adding poison pill.", e);
                             Thread.currentThread().interrupt();
                         }

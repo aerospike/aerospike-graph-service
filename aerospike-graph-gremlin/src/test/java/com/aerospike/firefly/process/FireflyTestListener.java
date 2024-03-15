5e94066b1d04d99713e7d6747225a0edb1608334
lyndon <lyndonb@apache.org>
2024-03-15 13:02:10 -0700

    GRAPH-1054 Rearraging things

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/FireflyTestListener.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/FireflyTestListener.java
@@ -8,7 +8,6 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class FireflyTestListener implements GraphProvider.TestListener {
-    private Instant start = Instant.now();
     private final Logger LOG;
 
     public FireflyTestListener(final Logger LOG) {
@@ -16,11 +15,10 @@ public class FireflyTestListener implements GraphProvider.TestListener {
     }
 
     public void onTestStart(final Class<?> test, final String testName) {
-        start = Instant.now();
         LOG.warn("===> Running " + testName + " <===");
     }
 
     public void onTestEnd(final Class<?> test, final String testName) {
-        LOG.warn("===> " + testName + " - " + Duration.between(start, Instant.now()).toMillis() + " ms <===");
+        LOG.warn("===> Completed " + testName + " <===");
     }
 }

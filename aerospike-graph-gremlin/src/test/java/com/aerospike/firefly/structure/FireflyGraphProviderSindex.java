e8621b0506b408f19a3ebfb0f6df57fdaee0790d
lyndon <lyndonb@apache.org>
2024-03-15 12:22:07 -0700

    GRAPH-1054 Rearraging things

---
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/structure/FireflyGraphProviderSindex.java
@@ -0,0 +1,80 @@
+package com.aerospike.firefly.structure;
+
+import com.aerospike.firefly.util.ConfigurationHelper;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.tinkerpop.gremlin.AbstractGraphProvider;
+import org.apache.tinkerpop.gremlin.LoadGraphWith;
+import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.EventStrategyProcessTest;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES;
+import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES_SINDEX;
+import static com.aerospike.firefly.util.ConfigurationHelper.Keys.ENABLE_FIREFLY_DROP_STRATEGY;
+
+/**
+ * @author Grant Haywood (<a href="http://iowntheinter.net">http://iowntheinter.net</a>)
+ */
+public class FireflyGraphProviderSindex extends AbstractGraphProvider {
+
+    private static final Configuration config;
+
+    static {
+        config = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES_SINDEX);
+    }
+
+    @Override
+    public Map<String, Object> getBaseConfiguration(final String graphName,
+                                                    final Class<?> test,
+                                                    final String testMethodName,
+                                                    final LoadGraphWith.GraphData loadGraphWith) {
+        // Load config map with base config
+        final HashMap<String, Object> configMap = new HashMap<>();
+        config.getKeys().forEachRemaining(key -> configMap.put(key, config.get(Object.class, key)));
+
+        // Add GRAPH_ID:graphName and GRAPH:FireflyGraph.
+        configMap.put(ConfigurationHelper.Keys.GRAPH_ID.toLowerCase(), graphName);
+        configMap.put(Graph.GRAPH, FireflyGraph.class.getName());
+
+        // Disable FireflyGraphDropStrategy for this test since it truncates the DB so event for vertex removal doesn't fire
+        if (test.equals(EventStrategyProcessTest.class) && testMethodName.equals("shouldTriggerRemoveVertex")) {
+            configMap.put(ENABLE_FIREFLY_DROP_STRATEGY.toLowerCase(), "false");
+        }
+
+        return configMap;
+    }
+
+    @Override
+    public void clear(final Graph graph, final Configuration configuration) {
+        if (graph != null) {
+            final FireflyGraph fireflyGraph = (FireflyGraph) graph;
+            if (fireflyGraph.closed.get()) {
+                try (final FireflyGraph fireflyGraph2 = FireflyGraph.open(configuration)) {
+                    fireflyGraph2.getBaseGraph().dropDatabase(fireflyGraph2, false);
+                }
+            } else {
+                fireflyGraph.getBaseGraph().dropDatabase(fireflyGraph, false);
+                fireflyGraph.close();
+            }
+        } else {
+            try (final FireflyGraph fireflyGraph = FireflyGraph.open(configuration)) {
+                fireflyGraph.getBaseGraph().dropDatabase(fireflyGraph, false);
+            }
+        }
+    }
+
+    @Override
+    public Set<Class> getImplementations() {
+        return new HashSet<>() {{
+            add(FireflyGraph.class);
+            add(FireflyGraphVariables.class);
+            add(FireflyEdge.class);
+            add(FireflyVertex.class);
+            add(FireflyVertexProperty.class);
+        }};
+    }
+}

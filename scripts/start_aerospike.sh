007676bc6543de983388c1835a74ba6c5c71722a
grant <grant@iowntheinter.net>
2024-03-18 20:20:38 -0600

    refactor script for env var

--- scripts/start_aerospike.sh
+++ scripts/start_aerospike.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 if [ ! -f .github/aerospike/features.conf ]; then
   if [ -z "$AEROSPIKE_FEATURES_B64" ]; then
-    echo "no features file present at .github/aerospike/features.conf"
+    echo "no AEROSPIKE_FEATURES_B64 env or features file present at .github/aerospike/features.conf"
+    exit
   else
     echo $AEROSPIKE_FEATURES_B64 | base64 -d > .github/aerospike/features.conf
   fi
-else
-  virtualenv -p $(which python3) .github/aerospike/venv
-  source .github/aerospike/venv/bin/activate
-  pip3 install -r .github/aerospike/requirements.txt
-  python3 .github/aerospike/start_cluster.py --features_file $(realpath .github/aerospike/features.conf) --repo_path $(realpath ./) $@
 fi
+virtualenv -p $(which python3) .github/aerospike/venv
+source .github/aerospike/venv/bin/activate
+pip3 install -r .github/aerospike/requirements.txt
+python3 .github/aerospike/start_cluster.py --features_file $(realpath .github/aerospike/features.conf) --repo_path $(realpath ./) $@
+

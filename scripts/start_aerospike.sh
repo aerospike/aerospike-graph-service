6392033b9477e5f190f73e2102076f05e601ac39
grant <grant@iowntheinter.net>
2024-03-18 20:11:41 -0600

    default version in python script

--- scripts/start_aerospike.sh
+++ scripts/start_aerospike.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-AS_VERSION=${1:-"ee-7.0.0.5_1"}
 if [ ! -f .github/aerospike/features.conf ]; then
   if [ -z "$AEROSPIKE_FEATURES_B64" ]; then
     echo "no features file present at .github/aerospike/features.conf"
@@ -10,5 +9,5 @@ else
   virtualenv -p $(which python3) .github/aerospike/venv
   source .github/aerospike/venv/bin/activate
   pip3 install -r .github/aerospike/requirements.txt
-  python3 .github/aerospike/start_cluster.py --aerospike_version $AS_VERSION --features_file $(realpath .github/aerospike/features.conf) --repo_path $(realpath ./) $@
+  python3 .github/aerospike/start_cluster.py --features_file $(realpath .github/aerospike/features.conf) --repo_path $(realpath ./) $@
 fi

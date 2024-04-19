9007d9575511d5b2ff474f142fdab136dbc51f57
DKZed <simondkz@protonmail.com>
2024-04-19 15:30:05 -0700

    Add logging for supernodes hit during traversals

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/map/FireflyVertexEdgeLocalCountStep.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/map/FireflyVertexEdgeLocalCountStep.java
@@ -1,5 +1,6 @@
 package com.aerospike.firefly.process.traversal.step.map;
 
+import com.aerospike.firefly.process.traversal.step.util.TraversalUtil;
 import com.aerospike.firefly.structure.FireflyVertex;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
@@ -27,6 +28,7 @@ public class FireflyVertexEdgeLocalCountStep extends MapStep<Vertex, Long> {
     protected Traverser.Admin<Long> processNextStart() throws NoSuchElementException {
         final Traverser.Admin<Vertex> traverser = this.starts.next();
         final FireflyVertex vertex = (FireflyVertex) traverser.get();
+        TraversalUtil.supernodeTraversalWarning(this.traversal, vertex);
         return traverser.split(vertex.getEdgeCount(direction), this);
     }
 }

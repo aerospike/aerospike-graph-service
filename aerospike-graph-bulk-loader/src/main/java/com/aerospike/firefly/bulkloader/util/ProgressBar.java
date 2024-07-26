fd9e75f7804694153f1ac6696aa9bafa6ddd693f
DKZed <simondkz@protonmail.com>
2024-07-26 16:02:13 -0700

    Remove negative edge write progress due to error handling

--- aerospike-graph-bulk-loader/src/main/java/com/aerospike/firefly/bulkloader/util/ProgressBar.java
+++ aerospike-graph-bulk-loader/src/main/java/com/aerospike/firefly/bulkloader/util/ProgressBar.java
@@ -25,6 +25,7 @@ public class ProgressBar extends TimerTask {
     private boolean edgeValidationComplete = false;
     private long verticesWritten = 0L;
     private long edgesWritten = 0L;
+    private long edgesWrittenHighWatermark = 0L;
     private long edgesInitial = 0L;
     private long verticesInitial = 0L;
     private int vertexPartitions = 0;
@@ -160,9 +161,22 @@ public class ProgressBar extends TimerTask {
                 return "\t\tEdge writing in progress\n";
             } else {
                 final long delta = updateAndGetDeltaEdgeCount(elementMetadata);
-                return "\t\tEdge writing in progress\n" +
-                        "\t\t\tWriting " + delta / (intervalMillis / 1000) + " edges per second\n" +
-                        "\t\t\tTotal of " + edgesWritten + " edges have been successfully written\n";
+                if (delta >= 0) {
+                    return "\t\tEdge writing in progress\n" +
+                            "\t\t\tWriting " + delta / (intervalMillis / 1000) + " edges per second\n" +
+                            "\t\t\tTotal of " + edgesWritten + " edges have been successfully written\n";
+                } else {
+                    final long badEdgeCount = this.graph.getBaseGraph().incrementAndGetBadEdgeCount(0);
+                    if (badEdgeCount >= edgesWrittenHighWatermark - edgesWritten) {
+                        return "\t\tEdge writing in progress\n" +
+                                "\t\t\tTotal of " + badEdgeCount + " edges detected as invalid and scheduled for purging\n" +
+                                "\t\t\tTotal of " + edgesWritten + " valid edges currently persist\n";
+                    } else {
+                        return "\t\tEdge writing in progress\n" +
+                                "\t\t\tError: Unexpected edge count decrease during a bulk load\n" +
+                                "\t\t\tEnsure that elements are not being concurrently removed via another source or contact support\n";
+                    }
+                }
                 // TODO: Can give % complete from edge partitions.
             }
         } else {
@@ -172,6 +186,7 @@ public class ProgressBar extends TimerTask {
 
     private long updateAndGetDeltaEdgeCount(final FireflyGraphSummaryUpdater.FireflyElementMetadata elementMetadata) {
         final long totalEdgeCount = elementMetadata.totalEdgeCount() - edgesInitial;
+        edgesWrittenHighWatermark = Math.max(edgesWrittenHighWatermark, totalEdgeCount);
         final long delta = totalEdgeCount - edgesWritten;
         edgesWritten = totalEdgeCount;
         return delta;

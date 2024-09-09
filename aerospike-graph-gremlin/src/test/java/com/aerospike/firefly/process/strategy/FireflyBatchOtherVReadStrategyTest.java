8a91b7001b9109428004e4e51c61fb74dd592721
Valentyn Kahamlyk <valentin.kagamlyk@gmail.com>
2024-09-09 08:31:02 -0700

    added tests

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/strategy/FireflyBatchOtherVReadStrategyTest.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/strategy/FireflyBatchOtherVReadStrategyTest.java
@@ -5,18 +5,20 @@ import com.aerospike.firefly.structure.FireflyGraph;
 import com.aerospike.firefly.util.ConfigurationHelper;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.tinkerpop.gremlin.GraphHelper;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.Step;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 
 import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES;
+import static org.junit.Assert.*;
 
 public class FireflyBatchOtherVReadStrategyTest {
     private static FireflyGraph graph;
@@ -43,10 +45,21 @@ public class FireflyBatchOtherVReadStrategyTest {
         final var g = graph.traversal();
 
         List result = g.V().outE().not(__.hasLabel("knows")).otherV().hasLabel("test").toList();
-        Assert.assertEquals(0, result.size());
+        assertEquals(0, result.size());
 
         result = g.V().outE().not(__.hasLabel("knows")).otherV().toList();
-        Assert.assertEquals(4, result.size());
+        assertEquals(4, result.size());
+
+        // borrowed from TinkerPop Feature tests
+        result = g.V().local(__.bothE("created").limit(1)).otherV().values("name").toList();
+        assertEquals(5, result.size());
+
+        result = g.V(4).bothE().otherV().toList();
+        assertEquals(3, result.size());
+
+        result = g.V(4).bothE().has("weight", P.lt(1.0)).otherV().toList();
+        assertEquals(1, result.size());
+        assertEquals(3, ((Vertex)result.get(0)).id());
     }
 
     @Test
@@ -56,7 +69,18 @@ public class FireflyBatchOtherVReadStrategyTest {
         var t = g.V().outE().not(__.hasLabel("knows")).otherV().hasLabel("test");
         t.asAdmin().applyStrategies();
         List<Step> steps = t.asAdmin().getSteps();
-        Assert.assertTrue(containsCustomStep(steps));
+        assertTrue(containsCustomStep(steps));
+    }
+
+    @Test
+    public void testStrategySkipped() {
+        final var g = graph.traversal();
+
+        // outE().otherV() is same as out(), so replaced with FireflyCompositeIdStep
+        var t = g.V().outE().otherV().hasLabel("test");
+        t.asAdmin().applyStrategies();
+        List<Step> steps = t.asAdmin().getSteps();
+        assertFalse(containsCustomStep(steps));
     }
 
     private boolean containsCustomStep(final List<Step> steps) {

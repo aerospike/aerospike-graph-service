0f56fc0e872e0ac5d66ef094348494935fd3516c
lyndon <lyndonb@apache.org>
2024-09-09 17:06:55 -0700

    GRAPH-792 Fix embedded strategies with group on root.

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/FireflyCompositeIdStep.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/FireflyCompositeIdStep.java
@@ -8,8 +8,10 @@ import com.aerospike.firefly.structure.FireflyVertex;
 import com.aerospike.firefly.structure.id.FireflyId;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
+import org.apache.tinkerpop.gremlin.process.traversal.step.LocalBarrier;
 import org.apache.tinkerpop.gremlin.process.traversal.step.util.CollectingBarrierStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer;
+import org.apache.tinkerpop.gremlin.process.traversal.traverser.ProjectedTraverser;
 import org.apache.tinkerpop.gremlin.process.traversal.traverser.util.TraverserSet;
 import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -26,7 +28,7 @@ import java.util.stream.Collectors;
 /**
  * @author Lyndon Bauto (<a href="https://github.com/lyndonbauto">https://github.com/lyndonbauto</a>)
  */
-public class FireflyCompositeIdStep extends CollectingBarrierStep<Vertex> {
+public class FireflyCompositeIdStep extends CollectingBarrierStep<Vertex> implements LocalBarrier<Vertex> {
     private final Direction direction;
     private final Set<String> edgeLabels;
 
@@ -35,6 +37,7 @@ public class FireflyCompositeIdStep extends CollectingBarrierStep<Vertex> {
     public final List<HasContainer> aerospikeHasContainers;
     private final int barrierSize;
     private final List<String> requiredProperties;
+    private boolean barrierConsumed = false;
 
     public FireflyCompositeIdStep(final Traversal.Admin traversal,
                                   final Direction direction,
@@ -110,5 +113,26 @@ public class FireflyCompositeIdStep extends CollectingBarrierStep<Vertex> {
 
         set.addAll(output);
         output.clear(); // Force garbage collection.
+    }
+
+    @Override
+    public void addBarrier(final TraverserSet<Vertex> barrier) {
+        barrier.forEach(traverser -> traverser.setSideEffects(this.getTraversal().getSideEffects()));
+        this.traverserSet.addAll(barrier);
+        this.barrierConsumed = false;
+    }
+
+    @Override
+    public Traverser.Admin<Vertex> processNextStart() {
+        if (this.traverserSet.isEmpty() && this.starts.hasNext()) {
+            this.processAllStarts();
+            this.barrierConsumed = false;
+        }
+        //
+        if (!this.barrierConsumed) {
+            this.barrierConsumer(this.traverserSet);
+            this.barrierConsumed = true;
+        }
+        return ProjectedTraverser.tryUnwrap(this.traverserSet.remove());
     }
 }
--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/strategy/optimization/FireflyBatchEdgeReadStrategy.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/strategy/optimization/FireflyBatchEdgeReadStrategy.java
@@ -25,13 +25,6 @@ import java.util.Set;
  */
 public class FireflyBatchEdgeReadStrategy extends FireflyStrategyBase {
 
-    final ThreadLocal<Boolean> rootGroup = new ThreadLocal<Boolean>() {
-        @Override
-        protected Boolean initialValue() {
-            return false;
-        }
-    };
-
     /**
      * Default constructor for FireflyBatchEdgeReadStrategy.
      */
@@ -47,15 +40,7 @@ public class FireflyBatchEdgeReadStrategy extends FireflyStrategyBase {
     public void apply(final Traversal.Admin<?, ?> traversal) {
         final FireflyGraph graph = (FireflyGraph) traversal.getGraph().get();
 
-        // Reset whenever root.
-        if (traversal.isRoot()) {
-            rootGroup.set(false);
-        }
-
         if (!traversal.isRoot()) {
-            if (rootGroup.get()) {
-                return;
-            }
             if (!graph.getBaseGraph().ENABLE_EMBEDDED_BATCH_EDGE_READ_STRATEGY) {
                 return;
             }
@@ -63,19 +48,8 @@ public class FireflyBatchEdgeReadStrategy extends FireflyStrategyBase {
 
         if (TraversalHelper.onGraphComputer(traversal))
             return;
-        final List<Step> steps = traversal.getSteps();
 
-        // TODO GRAPH-792: There's a weird interaction between the strategy and traversals like:
-        //  g.V().out().groupCount().by(outE().fold()).toList().
-        //  With these traversals there's a casting error that occurs at the end of the traversal pipe.
-        if (traversal.isRoot()) {
-            for (int i = 0; i < steps.size(); i++) {
-                if (steps.get(i) instanceof GroupStep || steps.get(i) instanceof GroupSideEffectStep) {
-                    rootGroup.set(true);
-                    break;
-                }
-            }
-        }
+        final List<Step> steps = traversal.getSteps();
 
         // We need to find VertexSteps.
         // In particular, we need vertex steps that return a vertex.

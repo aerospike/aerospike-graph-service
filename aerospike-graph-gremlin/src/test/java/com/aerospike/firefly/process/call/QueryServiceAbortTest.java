fd1f84045547437c156c545ea777c18f46917b64
lyndon <lyndonb@apache.org>
2024-09-11 10:27:51 -0700

    GRAPH-1291 Testing t3 machines.

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/call/QueryServiceAbortTest.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/call/QueryServiceAbortTest.java
@@ -100,7 +100,7 @@ public class QueryServiceAbortTest {
             threadSuccesses.add(assertion);
         }
         barrier.await();
-        Thread.sleep(100);
+        Thread.sleep(200);
         Map<String, Integer> queryAbortResult = (Map<String, Integer>) g.call("aerospike.graph.admin.query.abort").next();
         Assert.assertEquals(scanCount, (int) queryAbortResult.get("found"));
         Assert.assertEquals(scanCount, (int) queryAbortResult.get("aborted"));

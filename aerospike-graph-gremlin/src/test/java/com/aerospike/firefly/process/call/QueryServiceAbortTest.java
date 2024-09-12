2c47365440a1d95eaaa3cc72d3cd9a256c9f8a1c
lyndon <lyndonb@apache.org>
2024-09-12 12:39:58 -0700

    GRAPH-1291 trying smaller timeout

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/call/QueryServiceAbortTest.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/call/QueryServiceAbortTest.java
@@ -100,7 +100,7 @@ public class QueryServiceAbortTest {
             threadSuccesses.add(assertion);
         }
         barrier.await();
-        Thread.sleep(40);
+        Thread.sleep(5);
         Map<String, Integer> queryAbortResult = (Map<String, Integer>) g.call("aerospike.graph.admin.query.abort").next();
         Assert.assertEquals(scanCount, (int) queryAbortResult.get("found"));
         Assert.assertEquals(scanCount, (int) queryAbortResult.get("aborted"));

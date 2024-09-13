4330f9d673f97a37c1a90dc59eb943a3a9781d53
lyndon <lyndonb@apache.org>
2024-09-13 10:28:52 -0700

    fixing loop

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/computer/local/ComputerHelper.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/computer/local/ComputerHelper.java
@@ -2,11 +2,12 @@ package com.aerospike.firefly.process.computer.local;
 
 import org.apache.tinkerpop.gremlin.process.computer.traversal.step.map.TraversalVertexProgramStep;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.EmptyStep;
 
 public class ComputerHelper {
     public static boolean isComputerTraversal(final Traversal.Admin<?, ?> traversal) {
         Traversal.Admin<?, ?> t = traversal;
-        while (t.getParent() != null) {
+        while (!(t.getParent() instanceof EmptyStep)) {
             t = t.getParent().asStep().getTraversal();
         }
         if (!t.getSteps().isEmpty()) {

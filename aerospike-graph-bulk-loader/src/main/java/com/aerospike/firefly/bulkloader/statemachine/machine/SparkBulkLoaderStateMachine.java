2ffad61dc36914b0eddb7e3fe4275cc3782856fd
lyndon <lyndonb@apache.org>
2024-10-22 16:04:35 -0700

    GRAPH-1332 Added an additional check to vertex==edge directory.

--- aerospike-graph-bulk-loader/src/main/java/com/aerospike/firefly/bulkloader/statemachine/machine/SparkBulkLoaderStateMachine.java
+++ aerospike-graph-bulk-loader/src/main/java/com/aerospike/firefly/bulkloader/statemachine/machine/SparkBulkLoaderStateMachine.java
@@ -147,7 +147,10 @@ public class SparkBulkLoaderStateMachine {
 
             final String vertexDirectory = config.getOrDefault(VERTEX_DIRECTORY_KEY);
             final String edgeDirectory = config.getOrDefault(EDGE_DIRECTORY_KEY);
-            if (vertexDirectory.endsWith(".csv")) {
+            if (vertexDirectory.equals(edgeDirectory)) {
+                throw new IllegalArgumentException(String.format("Vertex and edge directories cannot be the same. Configs %s=%s and %s=%s.",
+                        VERTEX_DIRECTORY_KEY, vertexDirectory, EDGE_DIRECTORY_KEY, edgeDirectory));
+            } else if (vertexDirectory.endsWith(".csv")) {
                 throw new IllegalArgumentException("Config " + VERTEX_DIRECTORY_KEY + " must be a directory and cannot be a single file, current value is " + vertexDirectory + ".");
             } else if (edgeDirectory.endsWith(".csv")) {
                 throw new IllegalArgumentException("Config " + EDGE_DIRECTORY_KEY + " must be a directory and cannot be a single file, current value is " + edgeDirectory + ".");

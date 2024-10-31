94989e268d182ae4ff2f950c3c20e145376b47f1
lyndon <lyndonb@apache.org>
2024-10-31 11:23:25 -0700

    GRAPH-1335 working on scripts.

--- scripts/run-benchmark-gcp.sh
+++ scripts/run-benchmark-gcp.sh
@@ -14,7 +14,7 @@ pip3 install python_on_whales
 sudo tar -zxvf firefly.tgz
 
 # Build and Run Firefly Docker Image
-cd firefly && sudo python3 scripts/build-docker.py --tag firefly
+cd firefly && sudo python3 scripts/build-docker.py --tags firefly
 sudo docker run --name firefly -d -p 8182:8182 -p 9090:9090 -e aerospike.client.host="$(cat ./scripts/hosts.txt)" -e aerospike.graph.index.vertex.label.enabled=true -e aerospike.graph.index.vertex.properties=macAddress firefly
 
 # Run Benchmark

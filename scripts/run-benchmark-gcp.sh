9cae6463743dd6c17d423742cc18b251ed5802db
lyndon <lyndonb@apache.org>
2024-11-07 14:21:43 -0800

    test

--- scripts/run-benchmark-gcp.sh
+++ scripts/run-benchmark-gcp.sh
@@ -19,6 +19,7 @@ chmod +x ~/.docker/cli-plugins/docker-buildx
 sudo tar -zxvf firefly.tgz
 
 # Build and Run Firefly Docker Image
+sudo docker buildx create --use --driver docker-container
 cd firefly && sudo python3 scripts/build-docker.py --tags firefly
 sudo docker run --name firefly -d -p 8182:8182 -p 9090:9090 -e aerospike.client.host="$(cat ./scripts/hosts.txt)" -e aerospike.graph.index.vertex.label.enabled=true -e aerospike.graph.index.vertex.properties=macAddress firefly
 

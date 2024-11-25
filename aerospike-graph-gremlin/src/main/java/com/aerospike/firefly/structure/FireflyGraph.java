68af2ca748174a0c6669669ebd0f3a612107102f
lyndon <lyndonb@apache.org>
2024-11-25 15:42:56 -0800

    GRAPH-1392 Testing min == 0

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
@@ -377,10 +377,10 @@ public class FireflyGraph implements Graph, WrappedGraph<AerospikeConnection> {
         } catch (final Exception e) {
             LOG.error("=================== FAILED TO START AEROSPIKE GRAPH SERVICE ===================");
             LOG.error("========== Aerospike Graph Service failing to start is usually a result of an incorrect configuration.");
-            if (e.getMessage() != null) {
+            if (e.getMessage() != null && !"null".equals(e.getMessage())) {
                 LOG.error("========== See Error message for more details: {}", e.getMessage());
             } else {
-                LOG.error("========== Error did not contain message, please submit this stack trace to support: {}", e);
+                LOG.error("========== Error did not contain message, please submit this stack trace to support", e);
             }
 
             // Signal to gremlin-server to shut down.

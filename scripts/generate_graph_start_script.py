ee24ad3335aed4b2dc3669cdafac8c3e8a22f250
lyndon <lyndonb@apache.org>
2024-12-10 15:21:27 -0800

    GRAPH-1424 testing gcp benchmark

--- scripts/generate_graph_start_script.py
+++ scripts/generate_graph_start_script.py
@@ -11,7 +11,7 @@ def main(argv):
         properties.write("#!/usr/bin/bash\n")
         properties.write("set -eo pipefail\n")
         properties.write("sudo apt -y update\n")
-        properties.write("sudo apt install docker.io\n")
+        properties.write("sudo apt -y install docker.io\n")
         properties.write("echo {ghcr_io_login} | sudo docker login ghcr.io -u aerobot-firefly --password-stdin\n")
         properties.write(f"sudo docker run --name firefly -d -p 8182:8182 -p 9090:9090 -v $(pwd)/graph-config.properties:/opt/aerospike-graph/aerospike-graph.properties {ags_image}\n")
 

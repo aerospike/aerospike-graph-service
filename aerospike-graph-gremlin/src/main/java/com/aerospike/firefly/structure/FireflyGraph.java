e3ef3ff6ef5357ff7f6abf285f4c7676c083ed1f
lyndon <lyndonb@apache.org>
2025-01-13 11:50:13 -0800

    GRAPH-1375 Updating rest to stop extra creation

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/FireflyGraph.java
@@ -246,7 +246,6 @@ public class FireflyGraph implements Graph, WrappedGraph<AerospikeConnection> {
         this.idFactory = db.getIdFactory();
         this.bulkLoaderFlag = db.getBulkLoaderFlag();
         this.bulkLoadIdBufferSize = ConfigurationHelper.getOrDefaultInt(BULK_LOAD_ID_BUFFER_SIZE, conf);
-        final boolean bulkLoaderInitializerFlag = ConfigurationHelper.getOrDefaultBool(BULK_LOADER_INITIALIZER_FLAG, conf);
 
         this.variables = new FireflyGraphVariables(this);
         this.features = new FireflyFeatures();
@@ -257,14 +256,14 @@ public class FireflyGraph implements Graph, WrappedGraph<AerospikeConnection> {
         fireflyIndexMetadataTask.schedule(indexMetadataTimerTask, 0, db.INDEX_METADATA_UPDATE_FREQUENCY);
 
         // If bulk loading, only create indexes for the first bulk loader graph initialization. Otherwise they spam 1000's of times.
-        if (!bulkLoaderFlag || bulkLoaderInitializerFlag) {
+        if (db.shouldCreateIndexes()) {
             // Grab user defined vertex property indexes from the configuration and create them.
             final List<String> vertexPropertyIndexes = ConfigurationHelper.getOrDefaultList(ConfigurationHelper.Keys.VERTEX_PROPERTY_INDEXES, configuration);
             createIndexes(FireflyVertex.class, db.VERTEX_PROPERTY_NAME_TO_VALUE_BIN, db.getVpIndexPrefix(), vertexPropertyIndexes);
         }
 
         // If bulk loading, only create indexes for the first bulk loader graph initialization. Otherwise they spam 1000's of times.
-        if (!bulkLoaderFlag || bulkLoaderInitializerFlag) {
+        if (db.shouldCreateIndexes()) {
             // Grab user defined edge property indexes from the configuration and create them.
             final List<String> edgePropertyIndexes = ConfigurationHelper.getOrDefaultList(ConfigurationHelper.Keys.EDGE_PROPERTY_INDEXES, configuration);
             if (edgePropertyIndexes != null && !edgePropertyIndexes.isEmpty()) {

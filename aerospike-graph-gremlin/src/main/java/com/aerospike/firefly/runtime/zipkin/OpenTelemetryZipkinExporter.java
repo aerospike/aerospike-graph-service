2d69a6e5ab25188d69562b68eb8d9417508a4175
DKZed <simondkz@protonmail.com>
2025-01-21 10:43:59 -0800

    Support multi graph in query tracing

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/runtime/zipkin/OpenTelemetryZipkinExporter.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/runtime/zipkin/OpenTelemetryZipkinExporter.java
@@ -19,17 +19,16 @@ import org.apache.tinkerpop.gremlin.process.traversal.util.Metrics;
 import java.io.Closeable;
 import java.time.Instant;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class OpenTelemetryZipkinExporter implements Closeable {
     // Name of the service
     private static final String SERVICE_NAME = "ags-trace";
-    private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
-    private static final Random QUERY_SAMPLER = new Random();
-    private static OpenTelemetryZipkinExporter INSTANCE;
+    private static Map<String, OpenTelemetryZipkinExporter> INSTANCES = new ConcurrentHashMap<>();
 
     private static final String metricPercentDuration = "percentDur";
     private static final String displayPercentDuration = "PercentDuration";
@@ -38,15 +37,19 @@ public class OpenTelemetryZipkinExporter implements Closeable {
     private static final String metricElementCount = "elementCount";
     private static final String displayElementCount = "TotalElements";
 
+    private final Random querySampler = new Random();
     private final OpenTelemetry openTelemetry;
     private final SdkTracerProvider tracerProvider;
+    private final String graphId;
     private final int minQueryThresholdMillis;
     private final int samplingPercentage;
 
     private OpenTelemetryZipkinExporter(final OpenTelemetry openTelemetry, final SdkTracerProvider tracerProvider,
-                                        final int minQueryThresholdMillis, final int samplingPercentage) {
+                                        final String graphId, final int minQueryThresholdMillis,
+                                        final int samplingPercentage) {
         this.openTelemetry = openTelemetry;
         this.tracerProvider = tracerProvider;
+        this.graphId = graphId;
         this.minQueryThresholdMillis = minQueryThresholdMillis;
         this.samplingPercentage = samplingPercentage;
     }
@@ -54,16 +57,17 @@ public class OpenTelemetryZipkinExporter implements Closeable {
     /**
      * Adds a SimpleSpanProcessor initialized with ZipkinSpanExporter to the TracerSdkProvider
      */
-    static synchronized public OpenTelemetryZipkinExporter create(final String ip,
+    static synchronized public OpenTelemetryZipkinExporter create(final String graphId,
+                                                                  final String ip,
                                                                   final int port,
                                                                   final int minQueryThresholdMillis,
                                                                   final int samplingPercentage) {
-        if (!INITIALIZED.getAndSet(true)) {
+        if (!INSTANCES.containsKey(graphId)) {
             final String endpoint = String.format("http://%s:%s/api/v2/spans", ip, port);
             final ZipkinSpanExporter zipkinExporter = ZipkinSpanExporter.builder().setEndpoint(endpoint).build();
 
             final Resource serviceNameResource =
-                    Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, SERVICE_NAME));
+                    Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, SERVICE_NAME + " - " + graphId));
 
             // Set to process the spans by the Zipkin Exporter
             final SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
@@ -71,21 +75,21 @@ public class OpenTelemetryZipkinExporter implements Closeable {
                     .setResource(Resource.getDefault().merge(serviceNameResource))
                     .build();
             final OpenTelemetrySdk openTelemetry =
-                    OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).buildAndRegisterGlobal();
+                    OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
 
             // add a shutdown hook to shut down the SDK
             Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
-            INSTANCE = new OpenTelemetryZipkinExporter(openTelemetry, tracerProvider, minQueryThresholdMillis,
-                    samplingPercentage);
+            INSTANCES.put(graphId, new OpenTelemetryZipkinExporter(openTelemetry, tracerProvider, graphId,
+                    minQueryThresholdMillis, samplingPercentage));
         }
 
         // return the configured instance so it can be used for instrumentation.
-        return INSTANCE;
+        return INSTANCES.get(graphId);
     }
 
     public void exportQuery(final DefaultTraversalMetrics metrics, final String scopeName, final String traversal) {
         if (metrics.getDuration(TimeUnit.MILLISECONDS) >= this.minQueryThresholdMillis &&
-                QUERY_SAMPLER.nextInt(100) < this.samplingPercentage) {
+                querySampler.nextInt(100) < this.samplingPercentage) {
             final Tracer tracer = this.openTelemetry.getTracerProvider().get(scopeName);
             final SpanBuilder spanBuilder = tracer.spanBuilder(traversal);
 
@@ -139,8 +143,9 @@ public class OpenTelemetryZipkinExporter implements Closeable {
 
     @Override
     public synchronized void close() {
-        if (INITIALIZED.getAndSet(false)) {
+        if (INSTANCES.containsKey(this.graphId)) {
             this.tracerProvider.close();
+            INSTANCES.remove(graphId);
         }
     }
 }

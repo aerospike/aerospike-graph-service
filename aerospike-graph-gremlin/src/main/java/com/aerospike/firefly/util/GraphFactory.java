55d67aaf2f878df1797cd8378567978fbe74b2f2
Connor Hengstler <hengstler2005@gmail.com>
2025-05-08 17:06:38 -0700

    config option imlemented, starting test design

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/GraphFactory.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/GraphFactory.java
@@ -5,6 +5,7 @@ import com.aerospike.firefly.io.aerospike.DataModelVersioning;
 import com.aerospike.firefly.structure.FireflyGraph;
 import com.aerospike.firefly.util.config.ConfigurationHelper;
 import com.aerospike.firefly.util.config.FireflyConfiguration;
+import com.aerospike.firefly.util.exceptions.DataModelVersionMismatchException;
 import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,13 +24,24 @@ final public class GraphFactory {
             FireflyGraph.DATA_MODEL, FireflyGraph.class
     );
     private static final Logger LOG = LoggerFactory.getLogger(GraphFactory.class);
+
     public static FireflyGraph createGraph(final AerospikeConnection db, final FireflyConfiguration config) {
+
         final String dataModel = ConfigurationHelper.getOrDefaultString(FIREFLY_DATA_MODEL, config);
         if (!DATA_MODEL_MAP.containsKey(dataModel)) {
             throw new IllegalArgumentException("Unknown graph type: " + dataModel);
         } else {
             LOG.info("Constructing Graph for {} data model.", dataModel);
-            DataModelVersioning.checkVersionCompatibility(db);
+            try {
+                DataModelVersioning.checkVersionCompatibility(db);
+            } catch (DataModelVersionMismatchException e) {
+                if (db.CLEAR_ON_BUILD_ENABLED_FLAG) {
+                    LOG.info("Clearing graph...");
+                    db.clearNamespace(false);
+                } else {
+                    throw e;
+                }
+            }
             db.checkConfigurationCompatibility(config);
             return new FireflyGraph(db, config, getGremlinServerSettings());
         }
--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/io/TestDataModelVersioning.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/io/TestDataModelVersioning.java
@@ -1,10 +1,13 @@
 package com.aerospike.firefly.io;
 
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
 import com.aerospike.firefly.io.aerospike.AerospikeConnection;
 import com.aerospike.firefly.io.aerospike.DataModelVersioning;
 import com.aerospike.firefly.structure.FireflyGraph;
 import com.aerospike.firefly.util.config.ConfigurationHelper;
 import com.aerospike.firefly.util.exceptions.AerospikeGraphException;
+import com.aerospike.firefly.util.exceptions.DataModelVersionMismatchException;
 import com.aerospike.firefly.util.exceptions.GraphError;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.lang3.StringUtils;
@@ -23,6 +26,38 @@ public class TestDataModelVersioning {
     private static Configuration CONFIG = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
     private AerospikeConnection db;
 
+    @AfterClass
+    static public void afterAll() {
+        try (final AerospikeConnection db = AerospikeConnection.connect(CONFIG)) {
+            db.dropDatabase(null, false);
+        }
+    }
+
+    private static String getAdjustedVersion(final int versionIndex, final int delta) {
+        String fireflyVersion = FireflyGraph.FIREFLY_VERSION.toUpperCase();
+        if (fireflyVersion.endsWith("-SNAPSHOT")) {
+            fireflyVersion = fireflyVersion.replace("-SNAPSHOT", "");
+        }
+        String[] splitVersion = fireflyVersion.split("\\.");
+        // This isn't a problem since major version at time of writing is >= 2.
+        // For minor version, we only test by adjusting this positively.
+        // For patch version, this is the only time this can become negative when on x.x.0, but it doesn't matter since it doesn't affect compatibility.
+        splitVersion[versionIndex] = String.valueOf(Math.abs(Integer.parseInt(splitVersion[versionIndex]) + delta));
+        return StringUtils.join(splitVersion, '.');
+    }
+
+    private static String getAdjustedMajorVersion(final int delta) {
+        return getAdjustedVersion(0, delta);
+    }
+
+    private static String getAdjustedMinorVersion(final int delta) {
+        return getAdjustedVersion(1, delta);
+    }
+
+    private static String getAdjustedPatchVersion(final int delta) {
+        return getAdjustedVersion(2, delta);
+    }
+
     @Before
     public void beforeEach() {
         // Set graph to current version.
@@ -37,13 +72,6 @@ public class TestDataModelVersioning {
         // This can cause failures later if you don't clean it up.
         db.dropDatabase(null, false);
         db.close();
-    }
-
-    @AfterClass
-    static public void afterAll() {
-        try (final AerospikeConnection db = AerospikeConnection.connect(CONFIG)) {
-            db.dropDatabase(null, false);
-        }
     }
 
     @Test
@@ -90,28 +118,35 @@ public class TestDataModelVersioning {
         }
     }
 
-    private static String getAdjustedVersion(final int versionIndex, final int delta) {
-        String fireflyVersion = FireflyGraph.FIREFLY_VERSION.toUpperCase();
-        if (fireflyVersion.endsWith("-SNAPSHOT")) {
-            fireflyVersion = fireflyVersion.replace("-SNAPSHOT", "");
+    @Test
+    public void TestClearDataConfig() {
+        final Configuration CLEAR_CONFIG = ConfigurationHelper.loadFromFile("../../../resources/integration-test-settings-clear-on-build.properties");
+        //first set to a 2 level model and add data to server
+        db.setGraphMetadata("packed", "2.0.0");
+
+        Key key = new Key(db.getNamespace(), "demo", "user1");
+        Bin nameBin = new Bin("name", "alice");
+        Bin ageBin = new Bin("age", 30);
+        db.checkedPut(null, key, nameBin, ageBin);
+
+        //then change model to 3 level and try to start (throws error)
+        db.setGraphMetadata("packed", "3.0.0");
+        try {
+            final FireflyGraph graph = FireflyGraph.open(CONFIG);
+            Assert.fail("Should have failed due to model mismatch");
+        } catch (final DataModelVersionMismatchException e) {
+            Assert.assertEquals(GraphError.DATA_MODEL_VERSION_MISMATCH.code, e.errorCode);
         }
-        String[] splitVersion = fireflyVersion.split("\\.");
-        // This isn't a problem since major version at time of writing is >= 2.
-        // For minor version, we only test by adjusting this positively.
-        // For patch version, this is the only time this can become negative when on x.x.0, but it doesn't matter since it doesn't affect compatibility.
-        splitVersion[versionIndex] = String.valueOf(Math.abs(Integer.parseInt(splitVersion[versionIndex]) + delta));
-        return StringUtils.join(splitVersion, '.');
-    }
 
-    private static String getAdjustedMajorVersion(final int delta) {
-        return getAdjustedVersion(0, delta);
-    }
-
-    private static String getAdjustedMinorVersion(final int delta) {
-        return getAdjustedVersion(1, delta);
-    }
-
-    private static String getAdjustedPatchVersion(final int delta) {
-        return getAdjustedVersion(2, delta);
+        //then change config to clear enabled and try again
+        FireflyGraph clearGraph = null;
+        try {
+            clearGraph = FireflyGraph.open(CLEAR_CONFIG);
+            Assert.fail("Should have failed due to model mismatch");
+        } catch (Exception e) {
+            Assert.fail("Opening graph should not have thrown, but got: " + e);
+        }
+        Assert.assertNotNull("Graph instance should be non-null", clearGraph);
+        clearGraph.close();
     }
 }

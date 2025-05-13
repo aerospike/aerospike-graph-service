f5e20a5d9a8ab60eece9a3e58ba2575e69c7b41f
Connor Hengstler <hengstler2005@gmail.com>
2025-05-13 10:29:08 -0700

    Adding more verbose error message

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/exceptions/GraphError.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/util/exceptions/GraphError.java
@@ -99,7 +99,7 @@ public enum GraphError {
                 "Please disable it for all nodes in namespace.");
         ERROR_MESSAGES.put(NO_ACTIVE_NODES.code, "No active server nodes found in cluster.");
         ERROR_MESSAGES.put(DROP_INDEX_UNAUTHORIZED.code, "Failed to drop index due to role violation. Please check the permissions of the role assigned.");
-        ERROR_MESSAGES.put(DATA_MODEL_VERSION_MISMATCH.code, "The on-disk data model version '%s' is not compatible with the AGS version '%s' being used.");
+        ERROR_MESSAGES.put(DATA_MODEL_VERSION_MISMATCH.code, "The on-disk data model version '%s' is not compatible with the AGS version '%s' being used. To fix this, either use Aerospike Graph '%s', use a new namespace, or start with flag `aerospike.graph.clear.on.build.enabled` which will delete the old data and allow using the new model.");
         ERROR_MESSAGES.put(SINDEX_RECENTLY_DROPPED.code, "This query is temporarily unavailable due to the index it utilizes%s being recently dropped. Please wait %s seconds and try again.");
         ERROR_MESSAGES.put(TTL_ILLEGAL_ARGUMENT.code, "Invalid value for TTL provided. Provided input [%s] of type %s must be numeric instead.");
 

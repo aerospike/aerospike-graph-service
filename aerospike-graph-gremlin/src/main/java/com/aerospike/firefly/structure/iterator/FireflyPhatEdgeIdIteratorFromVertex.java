1dce132bf4e05b7fd56c0534f1663248b3fbe18e
DKZed <simondkz@protonmail.com>
2025-05-16 15:10:44 -0700

    Fixed phat edge iterator

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyPhatEdgeIdIteratorFromVertex.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/structure/iterator/FireflyPhatEdgeIdIteratorFromVertex.java
@@ -2,7 +2,9 @@ package com.aerospike.firefly.structure.iterator;
 
 import com.aerospike.client.Record;
 import com.aerospike.client.query.KeyRecord;
+import com.aerospike.firefly.io.FireflyEdgeRecord;
 import com.aerospike.firefly.io.aerospike.AerospikeConnection;
+import com.aerospike.firefly.structure.id.FireflyEdgeId;
 import com.aerospike.firefly.structure.id.FireflyId;
 import org.apache.tinkerpop.gremlin.process.traversal.util.FastNoSuchElementException;
 import org.apache.tinkerpop.gremlin.structure.Direction;
@@ -11,12 +13,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-
-import static com.aerospike.firefly.structure.FireflyEdge.IN_V_POSITION;
-import static com.aerospike.firefly.structure.FireflyEdge.LABEL_POSITION;
-import static com.aerospike.firefly.structure.FireflyEdge.OUT_V_POSITION;
 
 /**
  * @author Simon Zhao (<a href="https://www.linkedin.com/in/simonthezhao/</a>)
@@ -60,14 +57,15 @@ public abstract class FireflyPhatEdgeIdIteratorFromVertex extends FireflyPhatEdg
 
         while (outputIds.isEmpty() && this.keyRecords.hasNext()) {
             final Record record = this.keyRecords.next().record;
-            final Map<ByteBuffer, List<?>> edgeIdToData = (Map<ByteBuffer, List<?>>) record.getMap(db.EDGE_DATA_BIN);
+            final FireflyEdgeRecord edgeRecord = new FireflyEdgeRecord(record, db);
 
             if (this.direction == Direction.OUT || this.direction == Direction.BOTH) {
                 final Set<ByteBuffer> outEdgeIds = getIndividualEdgeIdsAttachedToVertex(record, Direction.OUT);
-                for (final ByteBuffer edgeId : outEdgeIds) {
-                    if (labels.isEmpty() || labels.contains((String) edgeIdToData.get(edgeId).get(LABEL_POSITION))) {
+                for (final ByteBuffer edgeIdBytes : outEdgeIds) {
+                    final FireflyEdgeId edgeId = this.db.getIdFactory().createEdgeId(edgeIdBytes);
+                    if (labels.isEmpty() || labels.contains(edgeRecord.getLabel(edgeId.getUniqueId()))) {
                         if (outputType == OutputType.VERTEX_ID) {
-                            final Object vertexId = edgeIdToData.get(edgeId).get(IN_V_POSITION);
+                            final FireflyId vertexId = edgeRecord.getInV(edgeId.getUniqueId());
                             outputIds.add(vertexId);
                         } else {
                             outputIds.add(edgeId);
@@ -78,10 +76,11 @@ public abstract class FireflyPhatEdgeIdIteratorFromVertex extends FireflyPhatEdg
 
             if (this.direction == Direction.IN || this.direction == Direction.BOTH) {
                 final Set<ByteBuffer> inEdgeIds = getIndividualEdgeIdsAttachedToVertex(record, Direction.IN);
-                for (final ByteBuffer edgeId : inEdgeIds) {
-                    if (labels.isEmpty() || labels.contains((String) edgeIdToData.get(edgeId).get(LABEL_POSITION))) {
+                for (final ByteBuffer edgeIdBytes : inEdgeIds) {
+                    final FireflyEdgeId edgeId = this.db.getIdFactory().createEdgeId(edgeIdBytes);
+                    if (labels.isEmpty() || labels.contains(edgeRecord.getLabel(edgeId.getUniqueId()))) {
                         if (outputType == OutputType.VERTEX_ID) {
-                            final Object vertexId = edgeIdToData.get(edgeId).get(OUT_V_POSITION);
+                            final Object vertexId = edgeRecord.getOutV(edgeId.getUniqueId());
                             outputIds.add(vertexId);
                         } else {
                             outputIds.add(edgeId);
@@ -104,11 +103,7 @@ public abstract class FireflyPhatEdgeIdIteratorFromVertex extends FireflyPhatEdg
             if (element == null) {
                 throw FastNoSuchElementException.instance();
             }
-            if (outputType.equals(OutputType.VERTEX_ID)) {
-                return this.db.getIdFactory().createVertexId(element);
-            } else {
-                return this.db.getIdFactory().createEdgeId(element);
-            }
+            return (FireflyId) element;
         } else {
             throw FastNoSuchElementException.instance();
         }

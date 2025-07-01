aa8ba3a964c63f2936f9e138dea3aca77599c2cc
Connor Hengstler <hengstler2005@gmail.com>
2025-07-01 11:54:17 -0700

    add tests for compress flag

---
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/performance/TestAerospikeNetwork.java
@@ -0,0 +1,154 @@
+package com.aerospike.firefly.performance;
+
+import com.aerospike.firefly.structure.FireflyGraph;
+import com.aerospike.firefly.util.config.ConfigurationHelper;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.StatisticNetworksConfig;
+import com.github.dockerjava.api.model.Statistics;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.core.InvocationBuilder;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import org.apache.commons.configuration2.Configuration;
+
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+
+import static com.aerospike.firefly.Tokens.INTEGRATION_TEST_PROPERTIES;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.CONTAINER_ID;
+
+public class TestAerospikeNetwork {
+    private Configuration uncompressConfig = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+    private Configuration compressConfig = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+    private FireflyGraph compressGraph;
+    private FireflyGraph uncompressGraph;
+    private DockerClient dockerClient;
+    private String containerId;
+
+    @Before
+    public void setUp() throws Exception {
+        dockerClient = initDockerClient();
+
+        Optional<String> matchedContainer = findAerospikeServerContainerId();
+        if (matchedContainer.isPresent()) {
+            containerId = matchedContainer.get();
+        } else {
+            throw new IllegalStateException("No container found with port 3000 bound to 0.0.0.0");
+        }
+
+        compressConfig.addProperty("aerospike.client.compress", true);
+        compressGraph = FireflyGraph.open(compressConfig);
+        uncompressGraph = FireflyGraph.open(uncompressConfig);
+
+        compressGraph.getBaseGraph().dropDatabase(compressGraph, false);
+        uncompressGraph.getBaseGraph().dropDatabase(uncompressGraph, false);
+    }
+
+    @Test
+    public void testCompressionFlag1KbChar() throws Exception {
+        System.out.println("Starting network monitoring for container: " + CONTAINER_ID);
+
+        String oneKbString = String.valueOf('a').repeat(1024);
+        uncompressGraph.traversal().addV("1KbA").property("goCrazy", oneKbString).next();
+
+        long[] uncompress = getNetworkStats(dockerClient, uncompressGraph);
+        long[] compress = getNetworkStats(dockerClient, compressGraph);
+
+        assertStatsReduced(uncompress, compress);
+    }
+
+    @Test
+    public void testCompressionFlag1KEdge() throws Exception {
+        final Configuration uncompressConfig = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        final Configuration compressConfig = ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES);
+        compressConfig.addProperty("aerospike.client.compress", true);
+
+        Vertex v1 = uncompressGraph.traversal().addV("1KbA").next();
+        for (int i = 0; i < 1000; i++) {
+            uncompressGraph.traversal().addE("is" + i).to(v1).from(v1).next();
+        }
+
+        long[] uncompress = getNetworkStats(dockerClient, uncompressGraph);
+        long[] compress = getNetworkStats(dockerClient, compressGraph);
+
+        assertStatsReduced(uncompress, compress);
+    }
+
+    private StatisticNetworksConfig captureStats() {
+        InvocationBuilder.AsyncResultCallback<Statistics> callback = new InvocationBuilder.AsyncResultCallback<>() {
+            @Override
+            public void onNext(Statistics stats) {
+                super.onNext(stats);
+                try {
+                    this.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        dockerClient.statsCmd(containerId).exec(callback);
+        return callback.awaitResult().getNetworks().values().iterator().next();
+    }
+
+    public long[] getNetworkStats(DockerClient dockerClient, FireflyGraph graph) throws InterruptedException {
+        StatisticNetworksConfig before = captureStats();
+        graph.traversal().V();
+        Thread.sleep(2000);
+        StatisticNetworksConfig after = captureStats();
+        return getNetworkDelta(before, after);
+    }
+
+    public long[] getNetworkDelta(StatisticNetworksConfig beforeStat, StatisticNetworksConfig afterStat) {
+        long[] after = new long[]{afterStat.getRxBytes(), afterStat.getTxBytes(), afterStat.getTxPackets()};
+        long[] before = new long[]{beforeStat.getRxBytes(), beforeStat.getTxBytes(), beforeStat.getTxPackets()};
+        return new long[]{after[0] - before[0], after[1] - before[1], after[2] - before[2]};
+    }
+
+    private void assertStatsReduced(long[] uncompressed, long[] compressed) {
+        System.out.printf("Uncompressed: Rx=%d, Tx=%d, Pkts=%d%n", uncompressed[0], uncompressed[1], uncompressed[2]);
+        System.out.printf("Compressed: Rx=%d, Tx=%d, Pkts=%d%n", compressed[0], compressed[1], compressed[2]);
+
+        Assert.assertTrue(uncompressed[0] > compressed[0]);
+        Assert.assertTrue(uncompressed[1] > compressed[1]);
+        Assert.assertTrue(uncompressed[2] > compressed[2]);
+    }
+
+    public Optional<String> findAerospikeServerContainerId() {
+        List<Container> containers = dockerClient.listContainersCmd().exec();
+        for (Container container : containers) {
+            if (container.getImage().contains("aerospike") && container.getImage().contains("server")) {
+                return Optional.of(container.getId());
+            }
+        }
+        return Optional.empty();
+    }
+
+    private DockerClient initDockerClient() {
+        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(30))
+                .build();
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dockerClient.close();
+        uncompressGraph.close();
+        compressGraph.close();
+    }
+}

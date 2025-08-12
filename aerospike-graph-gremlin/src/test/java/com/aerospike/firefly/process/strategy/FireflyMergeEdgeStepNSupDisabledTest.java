83d0387caa181484ea84730ea280f3f9c795d1af
lyndon <lyndonb@apache.org>
2025-08-12 12:37:14 -0700

    GRAPH-1622 test fix

--- aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/strategy/FireflyMergeEdgeStepNSupDisabledTest.java
+++ aerospike-graph-gremlin/src/test/java/com/aerospike/firefly/process/strategy/FireflyMergeEdgeStepNSupDisabledTest.java
@@ -19,6 +19,7 @@ public class FireflyMergeEdgeStepNSupDisabledTest {
     public void testNsupDisabledErrorMessage() {
         try (final FireflyGraph graph = FireflyGraph.open(ConfigurationHelper.loadFromFile(INTEGRATION_TEST_PROPERTIES))) {
             try {
+                graph.traversal().V().drop().iterate();
                 graph.traversal().addV().property(T.id, "vertex1").iterate();
                 graph.traversal().addV().property(T.id, "vertex2").iterate();
                 graph.traversal().mergeE(Map.of(

0f104af2256a5e7e82fbd1558edc6b3806e04477
lyndon <lyndonb@apache.org>
2025-08-21 17:51:40 -0700

    test

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/FireflyMergeEdgeStep.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/step/FireflyMergeEdgeStep.java
@@ -239,14 +239,18 @@ public class FireflyMergeEdgeStep<S> extends MergeStep<S, Edge, Object> {
         final FireflyId fromVId = fireflyGraph.getIdFactory().createVertexId(fromId);
         final FireflyId toVId = fireflyGraph.getIdFactory().createVertexId(toId);
         final List<FireflyVertex> fromVAndToV = fireflyGraph.readVertices(Collections.emptyList(),
-                List.of(fromVId), Collections.emptyList());
-        FireflyVertex fromV = fromVAndToV.get(0);
+                List.of(fromVId, toVId), Collections.emptyList());
+        FireflyVertex fromV = null;
+        FireflyVertex toV = null;
         for (final FireflyVertex vertex : fromVAndToV) {
             if (vertex.id.equals(fromVId)) {
                 fromV = vertex;
             }
+            if (vertex.id.equals(toVId)) {
+                toV = vertex;
+            }
         }
-        if (fromV == null) {
+        if (fromV == null || toV == null) {
             return CloseableIterator.of(Collections.emptyIterator());
         }
 
@@ -259,7 +263,12 @@ public class FireflyMergeEdgeStep<S> extends MergeStep<S, Edge, Object> {
             }
         }
 
-        return fromV.getEdgesAdjacentToVertex(Direction.OUT, toVId, edgeLabel, propertyFilters);
+        // If OUT/FROM is a supernode, just use IN/TO since it's a 50/50 which is faster if both are supernodes
+        if (fromV.isEdgeCacheOverflowed()) {
+            return toV.getEdgesAdjacentToVertex(Direction.IN, fromV.id, edgeLabel, propertyFilters);
+        } else {
+            return fromV.getEdgesAdjacentToVertex(Direction.OUT, toV.id, edgeLabel, propertyFilters);
+        }
     }
 
     protected Map<?,?> resolveVertices(final Map map, final Traverser.Admin<S> traverser) {

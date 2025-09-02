a2307e3d9efc4f9c067244a0f593801c8c6c7e40
Valentyn Kahamlyk <vkagamlyk@users.noreply.github.com>
2025-09-02 13:22:24 -0700

    GRAPH-1658 replaced tinkerpop ComputerVerificationStrategy (#1217)

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/strategy/verification/FireflyComputerVerificationStrategy.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/process/traversal/strategy/verification/FireflyComputerVerificationStrategy.java
@@ -1,28 +1,90 @@
 package com.aerospike.firefly.process.traversal.strategy.verification;
 
+import org.apache.tinkerpop.gremlin.process.computer.traversal.step.map.ComputerResultStep;
 import org.apache.tinkerpop.gremlin.process.computer.traversal.step.map.ShortestPathVertexProgramStep;
+import org.apache.tinkerpop.gremlin.process.computer.traversal.step.map.TraversalVertexProgramStep;
+import org.apache.tinkerpop.gremlin.process.computer.traversal.step.map.VertexProgramStep;
+import org.apache.tinkerpop.gremlin.process.traversal.Operator;
+import org.apache.tinkerpop.gremlin.process.traversal.Step;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategy;
+import org.apache.tinkerpop.gremlin.process.traversal.step.Mutating;
+import org.apache.tinkerpop.gremlin.process.traversal.step.PathProcessor;
+import org.apache.tinkerpop.gremlin.process.traversal.step.SideEffectCapable;
+import org.apache.tinkerpop.gremlin.process.traversal.step.map.ElementStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.map.GraphStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.sideEffect.InjectStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.sideEffect.IoStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.sideEffect.SubgraphStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.EmptyStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.ProfileStep;
 import org.apache.tinkerpop.gremlin.process.traversal.strategy.AbstractTraversalStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.strategy.verification.VerificationException;
+import org.apache.tinkerpop.gremlin.process.traversal.traverser.TraverserRequirement;
+import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalHelper;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.function.BinaryOperator;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public final class FireflyComputerVerificationStrategy extends AbstractTraversalStrategy<TraversalStrategy.VerificationStrategy> implements TraversalStrategy.VerificationStrategy {
     private static final FireflyComputerVerificationStrategy INSTANCE = new FireflyComputerVerificationStrategy();
-    private static final Set<Class<?>> UNSUPPORTED_ALGORITHM_STEPS = new HashSet<>(Arrays.asList(
-            ShortestPathVertexProgramStep.class));
+    private static final Set<Class<?>> UNSUPPORTED_ALGORITHM_STEPS = Set.of(ShortestPathVertexProgramStep.class);
+    private static final Set<Class<?>> UNSUPPORTED_STEPS = Set.of(
+            InjectStep.class, Mutating.class, SubgraphStep.class, ComputerResultStep.class, IoStep.class, ElementStep.class
+    );
+    private static final Set<Operator> UNSUPPORTED_OPERATORS = Set.of(Operator.minus, Operator.div);
 
     private FireflyComputerVerificationStrategy() {
     }
 
     @Override
     public void apply(final Traversal.Admin<?, ?> traversal) {
+        // copy-pasted from ComputerVerificationStrategy with some changes
+        if (traversal.getParent() instanceof TraversalVertexProgramStep) {
+            if (TraversalHelper.getStepsOfAssignableClassRecursively(GraphStep.class, traversal).size() > 1)
+                throw new VerificationException("Mid-traversal V()/E() is currently not supported on GraphComputer", traversal);
+            if (TraversalHelper.hasStepOfAssignableClassRecursively(ProfileStep.class, traversal) && TraversalHelper.getStepsOfAssignableClass(VertexProgramStep.class, TraversalHelper.getRootTraversal(traversal)).size() > 1)
+                throw new VerificationException("Profiling a multi-VertexProgramStep traversal is currently not supported on GraphComputer", traversal);
+        }
+
+        // this is a problem because sideEffect.merge() is transient on the OLAP reduction
+        if (TraversalHelper.getRootTraversal(traversal).getTraverserRequirements().contains(TraverserRequirement.ONE_BULK))
+            throw new VerificationException("One bulk is currently not supported on GraphComputer: " + traversal, traversal);
+
+//        // you can not traverse past the local star graph with localChildren (e.g. by()-modulators).
+//        if (!TraversalHelper.isGlobalChild(traversal) && !TraversalHelper.isLocalStarGraph(traversal))
+//            throw new VerificationException("Local traversals may not traverse past the local star-graph on GraphComputer: " + traversal, traversal);
+
+        for (final Step<?, ?> step : traversal.getSteps()) {
+            // there is issue with nested select step, let's keep this restriction for now
+            if (step instanceof PathProcessor && ((PathProcessor) step).getMaxRequirement() != PathProcessor.ElementRequirement.ID)
+                throw new VerificationException("It is not possible to access more than a path element's id on GraphComputer: " + step + " requires " + ((PathProcessor) step).getMaxRequirement(), traversal);
+
+            if (UNSUPPORTED_STEPS.stream().anyMatch(c -> c.isAssignableFrom(step.getClass()))
+                    // ComputerResultStep is only allowed as the final step of the root traversal
+                    && (!traversal.isRoot() || !(step instanceof ComputerResultStep)))
+                throw new VerificationException("The following step is currently not supported on GraphComputer: " + step, traversal);
+
+            if (step instanceof SideEffectCapable) {
+                final BinaryOperator<?> sideEffectOperator = traversal.getSideEffects().getReducer(((SideEffectCapable<?, ?>) step).getSideEffectKey());
+                if (UNSUPPORTED_OPERATORS.stream().anyMatch(o -> o == sideEffectOperator)) {
+                    throw new VerificationException("The following step has an SideEffect operator " + sideEffectOperator + " which is currently not supported on GraphComputer: " + step, traversal);
+                }
+            }
+        }
+
+        // // there is issue with nested select step, let's keep this restriction for now
+        Step<?, ?> nextParentStep = traversal.getParent().asStep();
+        while (!(nextParentStep instanceof EmptyStep)) {
+            if (nextParentStep instanceof PathProcessor && ((PathProcessor) nextParentStep).getMaxRequirement() != PathProcessor.ElementRequirement.ID)
+                throw new VerificationException("The following path processor step requires more than the element id on GraphComputer: " + nextParentStep + " requires " + ((PathProcessor) nextParentStep).getMaxRequirement(), traversal);
+            nextParentStep = nextParentStep.getNextStep();
+        }
+        // end of ComputerVerificationStrategy
+
         // ADDING A PRE-CHECK
         // TraversalVertexProgram is decomposed into various individual VertexPrograms when using the graph algorithms package
         traversal.getSteps().forEach(step -> {

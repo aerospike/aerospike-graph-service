fc586ec918ca6a3b39960c5256a400c9f93c1533
DKZed <simondkz@protonmail.com>
2025-09-17 15:57:38 -0700

    Update batch read function to support txn failure

--- aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/aerospike/AerospikeConnection.java
+++ aerospike-graph-gremlin/src/main/java/com/aerospike/firefly/io/aerospike/AerospikeConnection.java
@@ -2,6 +2,7 @@ package com.aerospike.firefly.io.aerospike;
 
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.AerospikeException;
+import com.aerospike.client.BatchRead;
 import com.aerospike.client.BatchRecord;
 import com.aerospike.client.Bin;
 import com.aerospike.client.Host;
@@ -29,7 +30,6 @@ import com.aerospike.client.exp.Exp;
 import com.aerospike.client.exp.ExpOperation;
 import com.aerospike.client.exp.ExpWriteFlags;
 import com.aerospike.client.exp.Expression;
-import com.aerospike.client.listener.RecordListener;
 import com.aerospike.client.listener.RecordSequenceListener;
 import com.aerospike.client.policy.AuthMode;
 import com.aerospike.client.policy.BatchPolicy;
@@ -1743,43 +1743,51 @@ public class AerospikeConnection implements AutoCloseable {
      * @param operations Read operations
      * @return Array of Record
      */
-    private Record[] batchRead(final Key[] keys, final BatchPolicy policy, final Operation[] operations, final FireflyCache cache) {
+    private Record[] batchRead(final Key[] keys, final BatchPolicy policy, final FireflyCache cache, final Operation... operations) {
         final BatchPolicy batchPolicy = policy == null ? new BatchPolicy() : policy;
         configureReadPolicy(batchPolicy);
         try {
-            return (cache != null) ? cache.read(keys, batchPolicy, operations) : client.get(batchPolicy, keys, operations);
+            if (cache != null) {
+                return cache.read(keys, batchPolicy, operations);
+            } else {
+                final List<BatchRecord> batchRecords = convertKeysForBatchRead(keys, operations);
+                final Record[] records = new Record[batchRecords.size()];
+                if (!this.client.operate(batchPolicy, batchRecords) && batchPolicy.txn != null) {
+                    for (int i = 0; i < batchRecords.size(); i++) {
+                        final BatchRecord batchRecord = batchRecords.get(i);
+                        if (GraphError.isTxnRelatedError(batchRecord.resultCode)) {
+                            final AerospikeException e = new AerospikeException(batchRecord.resultCode);
+                            LOG.error("Error: AerospikeException in read {}", e.getMessage());
+                            throw fromAerospikeException(e);
+                        }
+                        records[i] = batchRecord.record;
+                    }
+                } else {
+                    for (int i = 0; i < batchRecords.size(); i++) {
+                        records[i] = batchRecords.get(i).record;
+                    }
+                }
+                return records;
+            }
         } catch (final AerospikeException e) {
             LOG.error("Error: AerospikeException in read {}", e.getMessage());
             throw fromAerospikeException(e);
         }
     }
 
-    /**
-     * Perform a batch Aerospike read for a group of keys
-     *
-     * @param keys   Array of Key to return records for
-     * @param policy BatchPolicy to use
-     * @return Array of Record
-     */
-    private Record[] batchRead(final Key[] keys, final BatchPolicy policy, final FireflyCache cache) {
-        final BatchPolicy batchPolicy = policy == null ? new BatchPolicy() : policy;
-        configureReadPolicy(batchPolicy);
-        try {
-            return (cache != null) ? cache.read(keys, batchPolicy) : client.get(batchPolicy, keys);
-        } catch (final AerospikeException e) {
-            LOG.error("Error: AerospikeException in read {}", e.getMessage());
-            throw fromAerospikeException(e);
+    private static List<BatchRecord> convertKeysForBatchRead(final Key[] keys, final Operation... operations) {
+        final boolean readAllBins = operations.length == 0;
+        final List<BatchRecord> records = new ArrayList<>();
+        if (readAllBins) {
+            for (final Key key : keys) {
+                records.add(new BatchRead(key, true));
+            }
+        } else {
+            for (final Key key : keys) {
+                records.add(new BatchRead(key, operations));
+            }
         }
-    }
-
-    public void readWithListener(final RecordListener listener, final Policy policy, final Key key) {
-        final Policy readPolicy = policy == null ? new Policy() : policy;
-        configureReadPolicy(readPolicy);
-        try {
-            this.client.get(this.eventLoops.next(), listener, policy, key);
-        } catch (final AerospikeException e) {
-            throw fromAerospikeException(e);
-        }
+        return records;
     }
 
     /**
@@ -1825,29 +1833,28 @@ public class AerospikeConnection implements AutoCloseable {
      * @param operations Read operations
      * @return Array of Record
      */
-    Record[] skipCacheRead(final Key[] keys, final BatchPolicy policy, Operation[] operations) {
+    Record[] skipCacheRead(final Key[] keys, final BatchPolicy policy, final Operation... operations) {
         final BatchPolicy batchPolicy = policy == null ? new BatchPolicy() : policy;
         configureReadPolicy(batchPolicy);
         try {
-            return client.get(batchPolicy, keys, operations);
-        } catch (final AerospikeException e) {
-            throw fromAerospikeException(e);
-        }
-    }
-
-    /**
-     * Perform a batch Aerospike read for a group of keys and bypass the transaction cache. Should only be called by
-     * the cache.
-     *
-     * @param keys   Array of Key to return records for
-     * @param policy BatchPolicy to use
-     * @return Array of Record
-     */
-    Record[] skipCacheRead(final Key[] keys, final BatchPolicy policy) {
-        final BatchPolicy batchPolicy = policy == null ? new BatchPolicy() : policy;
-        configureReadPolicy(batchPolicy);
-        try {
-            return client.get(batchPolicy, keys);
+            final List<BatchRecord> batchRecords = convertKeysForBatchRead(keys, operations);
+            final Record[] records = new Record[batchRecords.size()];
+            if (!this.client.operate(batchPolicy, batchRecords) && batchPolicy.txn != null) {
+                for (int i = 0; i < batchRecords.size(); i++) {
+                    final BatchRecord batchRecord = batchRecords.get(i);
+                    if (GraphError.isTxnRelatedError(batchRecord.resultCode)) {
+                        final AerospikeException e = new AerospikeException(batchRecord.resultCode);
+                        LOG.error("Error: AerospikeException in read {}", e.getMessage());
+                        throw fromAerospikeException(e);
+                    }
+                    records[i] = batchRecord.record;
+                }
+            } else {
+                for (int i = 0; i < batchRecords.size(); i++) {
+                    records[i] = batchRecords.get(i).record;
+                }
+            }
+            return records;
         } catch (final AerospikeException e) {
             throw fromAerospikeException(e);
         }
@@ -1862,8 +1869,7 @@ public class AerospikeConnection implements AutoCloseable {
             // Default batch read used by read.
             final BatchPolicy batchReadPolicy = new BatchPolicy();
             batchReadPolicy.filterExp = filterExp;
-            return operations.length == 0 ? this.batchRead(keys, batchReadPolicy, cache) :
-                    this.batchRead(keys, batchReadPolicy, operations, cache);
+            return this.batchRead(keys, batchReadPolicy, cache, operations);
         } else {
             final WritePolicy policy = new WritePolicy();
             policy.filterExp = filterExp;
